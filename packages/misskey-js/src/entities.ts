06f0702ee8ca179d00c7cf316044ec2b18dc513e
okayurisotto <okayurisotto@proton.me>
2023-08-02 00:56:59 +0900

    misskey-js: `openapi-typescript`によって生成された型定義を使うように

--- packages/misskey-js/src/entities.ts
+++ packages/misskey-js/src/entities.ts
@@ -1,3 +1,5 @@
+import type { components } from './api.types';
+
 export type ID = string;
 export type DateString = string;
 
@@ -6,106 +8,15 @@ type TODO = Record<string, any>;
 // NOTE: 極力この型を使うのは避け、UserLite か UserDetailed か明示するように
 export type User = UserLite | UserDetailed;
 
-export type UserLite = {
-	id: ID;
-	username: string;
-	host: string | null;
-	name: string;
-	onlineStatus: 'online' | 'active' | 'offline' | 'unknown';
-	avatarUrl: string;
-	avatarBlurhash: string;
-	emojis: {
-		name: string;
-		url: string;
-	}[];
-	instance?: {
-		name: Instance['name'];
-		softwareName: Instance['softwareName'];
-		softwareVersion: Instance['softwareVersion'];
-		iconUrl: Instance['iconUrl'];
-		faviconUrl: Instance['faviconUrl'];
-		themeColor: Instance['themeColor'];
-	};
-};
+export type UserLite = components['schemas']['UserLite'];
 
-export type UserDetailed = UserLite & {
-	alsoKnownAs: string[];
-	bannerBlurhash: string | null;
-	bannerColor: string | null;
-	bannerUrl: string | null;
-	birthday: string | null;
-	createdAt: DateString;
-	description: string | null;
-	ffVisibility: 'public' | 'followers' | 'private';
-	fields: {name: string; value: string}[];
-	followersCount: number;
-	followingCount: number;
-	hasPendingFollowRequestFromYou: boolean;
-	hasPendingFollowRequestToYou: boolean;
-	isAdmin: boolean;
-	isBlocked: boolean;
-	isBlocking: boolean;
-	isBot: boolean;
-	isCat: boolean;
-	isFollowed: boolean;
-	isFollowing: boolean;
-	isLocked: boolean;
-	isModerator: boolean;
-	isMuted: boolean;
-	isSilenced: boolean;
-	isSuspended: boolean;
-	lang: string | null;
-	lastFetchedAt?: DateString;
-	location: string | null;
-	movedTo: string;
-	notesCount: number;
-	pinnedNoteIds: ID[];
-	pinnedNotes: Note[];
-	pinnedPage: Page | null;
-	pinnedPageId: string | null;
-	publicReactions: boolean;
-	securityKeys: boolean;
-	twoFactorEnabled: boolean;
-	updatedAt: DateString | null;
-	uri: string | null;
-	url: string | null;
-};
+export type UserDetailed = components['schemas']['UserDetailed'];
 
 export type UserGroup = TODO;
 
-export type UserList = {
-	id: ID;
-	createdAt: DateString;
-	name: string;
-	userIds: User['id'][];
-};
+export type UserList = components['schemas']['UserList'];
 
-export type MeDetailed = UserDetailed & {
-	avatarId: DriveFile['id'];
-	bannerId: DriveFile['id'];
-	autoAcceptFollowed: boolean;
-	alwaysMarkNsfw: boolean;
-	carefulBot: boolean;
-	emailNotificationTypes: string[];
-	hasPendingReceivedFollowRequest: boolean;
-	hasUnreadAnnouncement: boolean;
-	hasUnreadAntenna: boolean;
-	hasUnreadMentions: boolean;
-	hasUnreadMessagingMessage: boolean;
-	hasUnreadNotification: boolean;
-	hasUnreadSpecifiedNotes: boolean;
-	hideOnlineStatus: boolean;
-	injectFeaturedNote: boolean;
-	integrations: Record<string, any>;
-	isDeleted: boolean;
-	isExplorable: boolean;
-	mutedWords: string[][];
-	mutingNotificationTypes: string[];
-	noCrawle: boolean;
-	receiveAnnouncementEmail: boolean;
-	usePasswordLessLogin: boolean;
-	[other: string]: any;
-};
+export type MeDetailed = components['schemas']['MeDetailed'];
 
 export type MeDetailedWithSecret = MeDetailed & {
 	email: string;
@@ -121,141 +32,17 @@ export type MeSignup = MeDetailedWithSecret & {
 	token: string;
 };
 
-export type DriveFile = {
-	id: ID;
-	createdAt: DateString;
-	isSensitive: boolean;
-	name: string;
-	thumbnailUrl: string;
-	url: string;
-	type: string;
-	size: number;
-	md5: string;
-	blurhash: string;
-	comment: string | null;
-	properties: Record<string, any>;
-};
+export type DriveFile = components['schemas']['DriveFile'];
 
-export type DriveFolder = TODO;
+export type DriveFolder = components['schemas']['DriveFolder'];
 
-export type GalleryPost = {
-	id: ID;
-	createdAt: DateString;
-	updatedAt: DateString;
-	userId: User['id'];
-	user: User;
-	title: string;
-	description: string | null;
-	fileIds: DriveFile['id'][];
-	files: DriveFile[];
-	isSensitive: boolean;
-	likedCount: number;
-	isLiked?: boolean;
-};
+export type GalleryPost = components['schemas']['GalleryPost'];
 
-export type Note = {
-	id: ID;
-	createdAt: DateString;
-	text: string | null;
-	cw: string | null;
-	user: User;
-	userId: User['id'];
-	reply?: Note;
-	replyId: Note['id'];
-	renote?: Note;
-	renoteId: Note['id'];
-	files: DriveFile[];
-	fileIds: DriveFile['id'][];
-	visibility: 'public' | 'home' | 'followers' | 'specified';
-	visibleUserIds?: User['id'][];
-	localOnly?: boolean;
-	myReaction?: string;
-	reactions: Record<string, number>;
-	renoteCount: number;
-	repliesCount: number;
-	poll?: {
-		expiresAt: DateString | null;
-		multiple: boolean;
-		choices: {
-			isVoted: boolean;
-			text: string;
-			votes: number;
-		}[];
-	};
-	emojis: {
-		name: string;
-		url: string;
-	}[];
-	uri?: string;
-	url?: string;
-	isHidden?: boolean;
-};
+export type Note = components['schemas']['Note'];
 
-export type NoteReaction = {
-	id: ID;
-	createdAt: DateString;
-	user: UserLite;
-	type: string;
-};
+export type NoteReaction = components['schemas']['NoteReaction'];
 
-export type Notification = {
-	id: ID;
-	createdAt: DateString;
-	isRead: boolean;
-} & ({
-	type: 'reaction';
-	reaction: string;
-	user: User;
-	userId: User['id'];
-	note: Note;
-} | {
-	type: 'reply';
-	user: User;
-	userId: User['id'];
-	note: Note;
-} | {
-	type: 'renote';
-	user: User;
-	userId: User['id'];
-	note: Note;
-} | {
-	type: 'quote';
-	user: User;
-	userId: User['id'];
-	note: Note;
-} | {
-	type: 'mention';
-	user: User;
-	userId: User['id'];
-	note: Note;
-} | {
-	type: 'pollVote';
-	user: User;
-	userId: User['id'];
-	note: Note;
-} | {
-	type: 'follow';
-	user: User;
-	userId: User['id'];
-} | {
-	type: 'followRequestAccepted';
-	user: User;
-	userId: User['id'];
-} | {
-	type: 'receiveFollowRequest';
-	user: User;
-	userId: User['id'];
-} | {
-	type: 'groupInvited';
-	invitation: UserGroup;
-	user: User;
-	userId: User['id'];
-} | {
-	type: 'app';
-	header?: string | null;
-	body: string;
-	icon?: string | null;
-});
+export type Notification = components['schemas']['Notification'];
 
 export type MessagingMessage = {
 	id: ID;
@@ -371,27 +158,7 @@ export type Stats = {
 	driveUsageRemote: number;
 };
 
-export type Page = {
-	id: ID;
-	createdAt: DateString;
-	updatedAt: DateString;
-	userId: User['id'];
-	user: User;
-	content: Record<string, any>[];
-	variables: Record<string, any>[];
-	title: string;
-	name: string;
-	summary: string | null;
-	hideTitleWhenPinned: boolean;
-	alignCenter: boolean;
-	font: string;
-	script: string;
-	eyeCatchingImageId: DriveFile['id'] | null;
-	eyeCatchingImage: DriveFile | null;
-	attachedFiles: any;
-	likedCount: number;
-	isLiked?: boolean;
-};
+export type Page = components['schemas']['Page'];
 
 export type PageEvent = {
 	pageId: Page['id'];
@@ -411,24 +178,9 @@ export type Announcement = {
 	isRead?: boolean;
 };
 
-export type Antenna = {
-	id: ID;
-	createdAt: DateString;
-	name: string;
-	keywords: string[][]; // TODO
-	excludeKeywords: string[][]; // TODO
-	src: 'home' | 'all' | 'users' | 'list' | 'group';
-	userListId: ID | null; // TODO
-	userGroupId: ID | null; // TODO
-	users: string[]; // TODO
-	caseSensitive: boolean;
-	notify: boolean;
-	withReplies: boolean;
-	withFile: boolean;
-	hasUnreadNote: boolean;
-};
+export type Antenna = components['schemas']['Antenna'];
 
-export type App = TODO;
+export type App = components['schemas']['App'];
 
 export type AuthSession = {
 	id: ID;
@@ -438,14 +190,9 @@ export type AuthSession = {
 
 export type Ad = TODO;
 
-export type Clip = TODO;
+export type Clip = components['schemas']['Clip'];
 
-export type NoteFavorite = {
-	id: ID;
-	createdAt: DateString;
-	noteId: Note['id'];
-	note: Note;
-};
+export type NoteFavorite = components['schemas']['NoteFavorite'];
 
 export type FollowRequest = {
 	id: ID;
@@ -453,61 +200,17 @@ export type FollowRequest = {
 	followee: User;
 };
 
-export type Channel = {
-	id: ID;
-	// TODO
-};
+export type Channel = components['schemas']['Channel'];
 
-export type Following = {
-	id: ID;
-	createdAt: DateString;
-	followerId: User['id'];
-	followeeId: User['id'];
-};
+export type Following = components['schemas']['Following'];
 
-export type FollowingFolloweePopulated = Following & {
-	followee: UserDetailed;
-};
+export type FollowingFolloweePopulated = Following;
 
-export type FollowingFollowerPopulated = Following & {
-	follower: UserDetailed;
-};
+export type FollowingFollowerPopulated = Following;
 
-export type Blocking = {
-	id: ID;
-	createdAt: DateString;
-	blockeeId: User['id'];
-	blockee: UserDetailed;
-};
+export type Blocking = components['schemas']['Blocking'];
 
-export type Instance = {
-	id: ID;
-	caughtAt: DateString;
-	host: string;
-	usersCount: number;
-	notesCount: number;
-	followingCount: number;
-	followersCount: number;
-	driveUsage: number;
-	driveFiles: number;
-	latestRequestSentAt: DateString | null;
-	latestStatus: number | null;
-	latestRequestReceivedAt: DateString | null;
-	lastCommunicatedAt: DateString;
-	isNotResponding: boolean;
-	isSuspended: boolean;
-	softwareName: string | null;
-	softwareVersion: string | null;
-	openRegistrations: boolean | null;
-	name: string | null;
-	description: string | null;
-	maintainerName: string | null;
-	maintainerEmail: string | null;
-	iconUrl: string | null;
-	faviconUrl: string | null;
-	themeColor: string | null;
-	infoUpdatedAt: DateString | null;
-};
+export type Instance = components['schemas']['FederationInstance'];
 
 export type Signin = {
 	id: ID;
@@ -517,20 +220,11 @@ export type Signin = {
 	success: boolean;
 };
 
-export type Invite = {
-	id: ID;
-	code: string;
-	expiresAt: DateString | null;
-	createdAt: DateString;
-	createdBy: UserLite | null;
-	usedBy: UserLite | null;
-	usedAt: DateString | null;
-	used: boolean;
-}
+export type Invite = components['schemas']['InviteCode'];
 
 export type InviteLimit = {
 	remaining: number;
-}
+};
 
 export type UserSorting =
 	| '+follower'
@@ -539,4 +233,5 @@ export type UserSorting =
 	| '-createdAt'
 	| '+updatedAt'
 	| '-updatedAt';
+
 export type OriginType = 'combined' | 'local' | 'remote';
---
+++ packages/misskey-js/src/wrapper.ts
@@ -0,0 +1,18 @@
+import { paths } from './api.types';
+
+type RemoveSlash<T extends `/${string}`> = T extends `/${infer U}` ? U : never;
+
+export type Endpoints = {
+	[K in RemoveSlash<keyof paths>]: {
+		req: paths[`/${K}`]['post']['requestBody']['content'] extends {
+			'application/json': Record<string, unknown>;
+		}
+			? paths[`/${K}`]['post']['requestBody']['content']['application/json']
+			: Record<string, never>;
+		res: paths[`/${K}`]['post']['responses'] extends {
+			'200': { content: { 'application/json': Record<string, unknown> } };
+		}
+			? paths[`/${K}`]['post']['responses']['200']['content']['application/json']
+			: Record<string, never>;
+	};
+};
