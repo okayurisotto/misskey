cbd0c887b8f6e5be95cbb19b468f69ea72c78029
okayurisotto <okayurisotto@proton.me>
2023-08-04 14:24:28 +0900

    zod2spec: cleanup

--- packages/zod2spec/type/index.d.ts
+++
@@ -1,8 +0,0 @@
-import type { ReferenceObject, SchemaObject } from "openapi3-ts/oas30";
-import { key } from "./const.js";
-import { OpenApiZod } from "./type.js";
-export declare const defineOpenApiSpec: <T extends OpenApiZod>(schema: T, spec: SchemaObject | ReferenceObject) => T;
-export declare const generateOpenApiSpec: (components: {
-    key: string;
-    schema: OpenApiZod;
-}[]) => (schema: OpenApiZod) => SchemaObject | ReferenceObject;
