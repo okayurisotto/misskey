acc09c4f0395410901def4086f7980846fa8d321
okayurisotto <okayurisotto@proton.me>
2023-08-13 15:04:28 +0900

    frontend: misskey-jsの自動生成された型定義を使うように

--- packages/frontend/src/scripts/api.ts
+++ packages/frontend/src/scripts/api.ts
@@ -1,4 +1,4 @@
-import { Endpoints } from 'misskey-js/built/api.types';
+import { Endpoints } from 'misskey-js/src/wrapper';
 import { ref } from 'vue';
 import { apiUrl } from '@/config';
 import { $i } from '@/account';
