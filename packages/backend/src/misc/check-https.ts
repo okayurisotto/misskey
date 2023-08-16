89a7f4f8d6f4e6da7d06b484aa95078ac864c0de
okayurisotto <okayurisotto@proton.me>
2023-08-16 19:06:54 +0900

    backend: リファクタリングとJSDoc

--- packages/backend/src/misc/check-https.ts
+++ packages/backend/src/misc/check-https.ts
@@ -1,4 +1,12 @@
+/**
+ * URLがHTTPSであるか確認する。
+ * `NODE_ENV`が`"production"`出なかった場合はHTTPも許容する。
+ *
+ * @param url
+ * @returns
+ */
 export function checkHttps(url: string): boolean {
-	return url.startsWith('https://') ||
-		(url.startsWith('http://') && process.env["NODE_ENV"] !== 'production');
+	if (url.startsWith('https://')) return true;
+	if (url.startsWith('http://') && process.env['NODE_ENV'] !== 'production') return true;
+	return false;
 }
