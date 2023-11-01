de584c155eedb40a91c40ac5dd7827d38d90677d
okayurisotto <okayurisotto@proton.me>
2023-11-01 09:05:39 +0900

    backend: refactor `DeleteAccountService.ts`

--- packages/backend/src/core/DeleteAccountService.ts
+++ packages/backend/src/core/DeleteAccountService.ts
@@ -3,6 +3,7 @@ import { QueueService } from '@/core/QueueService.js';
 import { UserSuspendService } from '@/core/UserSuspendService.js';
 import { bindThis } from '@/decorators.js';
 import { PrismaService } from '@/core/PrismaService.js';
+import type { user } from '@prisma/client';
 
 @Injectable()
 export class DeleteAccountService {
@@ -13,17 +14,19 @@ export class DeleteAccountService {
 	) {}
 
 	@bindThis
-	public async deleteAccount(user: {
-		id: string;
-		host: string | null;
-	}): Promise<void> {
-		const user_ = await this.prismaService.client.user.findUniqueOrThrow({ where: { id: user.id } });
-		if (user_.isRoot) throw new Error('cannot delete a root account');
+	public async deleteAccount(user: Pick<user, 'id' | 'host'>): Promise<void> {
+		const isRoot = await this.prismaService.client.user
+			.count({
+				where: { id: user.id, isRoot: true },
+				take: 1,
+			})
+			.then((count) => count !== 0);
+		if (isRoot) throw new Error('cannot delete a root account');
 
 		// 物理削除する前にDelete activityを送信する
 		await this.userSuspendService.doPostSuspend(user).catch(() => {});
 
-		this.queueService.createDeleteAccountJob(user, { soft: false });
+		await this.queueService.createDeleteAccountJob(user, { soft: false });
 
 		await this.prismaService.client.user.update({
 			where: { id: user.id },
