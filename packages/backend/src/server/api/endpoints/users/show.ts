d723909465cbf807f12b77c2c58ff1ecaa9fe425
okayurisotto <okayurisotto@proton.me>
2023-11-01 15:32:43 +0900

    backend: refactor `/users/show`
    `paramDef`を実際の処理に合った形に変更した。

--- packages/backend/src/server/api/endpoints/users/show.ts
+++ packages/backend/src/server/api/endpoints/users/show.ts
@@ -14,7 +14,6 @@ import { UserDetailedSchema } from '@/models/zod/UserDetailedSchema.js';
 import { PrismaService } from '@/core/PrismaService.js';
 import { ApiError } from '../../error.js';
 import { ApiLoggerService } from '../../ApiLoggerService.js';
-import type { user } from '@prisma/client';
 
 const res = z.union([UserDetailedSchema, z.array(UserDetailedSchema)]);
 export const meta = {
@@ -28,29 +27,17 @@ export const meta = {
 	},
 } as const;
 
-const paramDef_base = {
-	host: z
-		.string()
-		.nullable()
-		.optional()
-		.describe('The local host is represented with `null`.'),
-};
 export const paramDef = z.union([
-	z.object(paramDef_base).merge(
-		z.object({
-			userId: MisskeyIdSchema,
-		}),
-	),
-	z.object(paramDef_base).merge(
-		z.object({
-			userIds: uniqueItems(z.array(MisskeyIdSchema)),
-		}),
-	),
-	z.object(paramDef_base).merge(
-		z.object({
-			username: z.string(),
-		}),
-	),
+	z.object({ userIds: uniqueItems(z.array(MisskeyIdSchema)) }),
+	z.object({ userId: MisskeyIdSchema }),
+	z.object({
+		host: z
+			.string()
+			.nullable()
+			.default(null)
+			.describe('The local host is represented with `null`.'),
+		username: z.string(),
+	}),
 ]);
 
 @Injectable()
@@ -71,10 +58,66 @@ export default class extends Endpoint<
 		super(meta, paramDef, async (ps, me, _1, _2, _3, ip) => {
 			const isModerator = await this.roleService.isModerator(me);
 
-			if ('userIds' in ps) {
-				if (ps.userIds.length === 0) {
-					return [];
+			if ('userId' in ps) {
+				const user = await this.prismaService.client.user.findUnique({
+					where: {
+						id: ps.userId,
+						...(isModerator ? {} : { isSuspended: false }),
+					},
+				});
+
+				if (user === null) {
+					throw new ApiError(meta.errors.noSuchUser);
 				}
+
+				return await this.userEntityService.packDetailed(user, me);
+			}
+
+			if ('username' in ps) {
+				if (ps.host === null) {
+					const user = await this.prismaService.client.user.findFirst({
+						where: {
+							usernameLower: ps.username.trim().toLowerCase(),
+							host: null,
+							...(isModerator ? {} : { isSuspended: false }),
+						},
+					});
+
+					if (user === null) {
+						throw new ApiError(meta.errors.noSuchUser);
+					}
+
+					if (me === null) {
+						if (ip != null) {
+							this.perUserPvChart.commitByVisitor(user, ip);
+						}
+					} else {
+						if (me.id !== user.id) {
+							this.perUserPvChart.commitByUser(user, me.id);
+						}
+					}
+
+					return await this.userEntityService.packDetailed(user, me);
+				} else {
+					const user = await this.remoteUserResolveService
+						.resolveUser(ps.username.trim(), ps.host)
+						.catch((err) => {
+							this.apiLoggerService.logger.warn(
+								`failed to resolve remote user: ${err}`,
+							);
+							throw new ApiError(meta.errors.failedToResolveRemoteUser);
+						});
+
+					if (!isModerator && user.isSuspended) {
+						throw new ApiError(meta.errors.noSuchUser);
+					}
+
+					return await this.userEntityService.packDetailed(user, me);
+				}
+			}
+
+			if ('userIds' in ps) {
+				if (ps.userIds.length === 0) return [];
 
 				const users = await this.prismaService.client.user.findMany({
 					where: isModerator
@@ -82,51 +125,21 @@ export default class extends Endpoint<
 						: { id: { in: ps.userIds }, isSuspended: false },
 				});
 
-				// リクエストされた通りに並べ替え
-				const users_: user[] = [];
-				for (const id of ps.userIds) {
-					users_.push(users.find((x) => x.id === id)!);
-				}
-
 				return await Promise.all(
-					users_.map((u) => this.userEntityService.packDetailed(u, me)),
+					ps.userIds
+						// リクエストされた通りに並べ替え
+						.map((id) => {
+							const user = users.find((user) => user.id === id);
+							if (user === undefined) {
+								throw new ApiError(meta.errors.noSuchUser);
+							}
+							return user;
+						})
+						.map((user) => this.userEntityService.packDetailed(user, me)),
 				);
 			}
 
-			let user: user | null;
-
-			// Lookup user
-			if (ps.host != null && 'username' in ps) {
-				user = await this.remoteUserResolveService
-					.resolveUser(ps.username.trim(), ps.host)
-					.catch((err) => {
-						this.apiLoggerService.logger.warn(
-							`failed to resolve remote user: ${err}`,
-						);
-						throw new ApiError(meta.errors.failedToResolveRemoteUser);
-					});
-			} else {
-				user = await this.prismaService.client.user.findFirst({
-					where:
-						'username' in ps
-							? { usernameLower: ps.username.trim().toLowerCase(), host: null }
-							: { id: ps.userId },
-				});
-			}
-
-			if (user == null || (!isModerator && user.isSuspended)) {
-				throw new ApiError(meta.errors.noSuchUser);
-			}
-
-			if (user.host == null) {
-				if (me == null && ip != null) {
-					this.perUserPvChart.commitByVisitor(user, ip);
-				} else if (me && me.id !== user.id) {
-					this.perUserPvChart.commitByUser(user, me.id);
-				}
-			}
-
-			return await this.userEntityService.packDetailed(user, me);
+			return ps satisfies never;
 		});
 	}
 }
