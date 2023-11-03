6437022176f358f310c6c8040996a85a49af84c9
okayurisotto <okayurisotto@proton.me>
2023-11-04 02:37:44 +0900

    backend: refactor `/clips/favorite`

--- packages/backend/src/server/api/endpoints/clips/favorite.ts
+++ packages/backend/src/server/api/endpoints/clips/favorite.ts
@@ -1,18 +1,15 @@
 import { z } from 'zod';
 import { Injectable } from '@nestjs/common';
-import { noSuchClip__, alreadyFavorited } from '@/server/api/errors.js';
 import { IdService } from '@/core/IdService.js';
 import { Endpoint } from '@/server/api/abstract-endpoint.js';
 import { MisskeyIdSchema } from '@/models/zod/misc.js';
 import { PrismaService } from '@/core/PrismaService.js';
-import { ApiError } from '../../error.js';
 
 export const meta = {
 	tags: ['clip'],
 	requireCredential: true,
 	prohibitMoved: true,
 	kind: 'write:clip-favorite',
-	errors: {noSuchClip:noSuchClip__,alreadyFavorited:alreadyFavorited},
 } as const;
 
 export const paramDef = z.object({ clipId: MisskeyIdSchema });
@@ -29,35 +26,17 @@ export default class extends Endpoint<
 		private readonly prismaService: PrismaService,
 	) {
 		super(meta, paramDef, async (ps, me) => {
-			const clip = await this.prismaService.client.clip.findUnique({
-				where: { id: ps.clipId },
-			});
-			if (clip == null) {
-				throw new ApiError(meta.errors.noSuchClip);
-			}
-			if (clip.userId !== me.id && !clip.isPublic) {
-				throw new ApiError(meta.errors.noSuchClip);
-			}
-
-			const exist =
-				(await this.prismaService.client.clip_favorite.count({
-					where: {
-						clipId: clip.id,
-						userId: me.id,
-					},
-					take: 1,
-				})) > 0;
-
-			if (exist) {
-				throw new ApiError(meta.errors.alreadyFavorited);
-			}
-
 			await this.prismaService.client.clip_favorite.create({
 				data: {
 					id: this.idService.genId(),
 					createdAt: new Date(),
-					clipId: clip.id,
-					userId: me.id,
+					clip: {
+						connect: {
+							id: ps.clipId,
+							OR: [{ isPublic: true }, { userId: me.id }],
+						},
+					},
+					user: { connect: { id: me.id } },
 				},
 			});
 		});
