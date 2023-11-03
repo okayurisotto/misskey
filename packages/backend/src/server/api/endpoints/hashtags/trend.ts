a60b101afa1bd6ae25248ec8426000f1eecd28d1
okayurisotto <okayurisotto@proton.me>
2023-11-04 02:14:22 +0900

    backend: refactor `/hashtags/trend`

--- packages/backend/src/server/api/endpoints/hashtags/trend.ts
+++ packages/backend/src/server/api/endpoints/hashtags/trend.ts
@@ -4,14 +4,13 @@ import { Endpoint } from '@/server/api/abstract-endpoint.js';
 import { normalizeForSearch } from '@/misc/normalize-for-search.js';
 import { MetaService } from '@/core/MetaService.js';
 import { PrismaService } from '@/core/PrismaService.js';
-import type { note } from '@prisma/client';
 
 /*
-トレンドに載るためには「『直近a分間のユニーク投稿数が今からa分前～今からb分前の間のユニーク投稿数のn倍以上』のハッシュタグの上位5位以内に入る」ことが必要
-ユニーク投稿数とはそのハッシュタグと投稿ユーザーのペアのカウントで、例えば同じユーザーが複数回同じハッシュタグを投稿してもそのハッシュタグのユニーク投稿数は1とカウントされる
-
-..が理想だけどPostgreSQLでどうするのか分からないので単に「直近Aの内に投稿されたユニーク投稿数が多いハッシュタグ」で妥協する
-*/
+ * トレンドに載るためには「『直近a分間のユニーク投稿数が今からa分前～今からb分前の間のユニーク投稿数のn倍以上』のハッシュタグの上位5位以内に入る」ことが必要
+ * ユニーク投稿数とはそのハッシュタグと投稿ユーザーのペアのカウントで、例えば同じユーザーが複数回同じハッシュタグを投稿してもそのハッシュタグのユニーク投稿数は1とカウントされる
+ *
+ * ..が理想だけどPostgreSQLでどうするのか分からないので単に「直近Aの内に投稿されたユニーク投稿数が多いハッシュタグ」で妥協する
+ */
 
 const rangeA = 1000 * 60 * 60; // 60分
 //const rangeB = 1000 * 60 * 120; // 2時間
@@ -67,90 +66,70 @@ export default class extends Endpoint<
 				return [];
 			}
 
-			const tags: {
-				name: string;
-				users: note['userId'][];
-			}[] = [];
+			const tags = [
+				...tagNotes
+					.map((note) => note.tags.map((tag) => ({ tag, userId: note.userId })))
+					.flat()
+					.filter((entry) => !hiddenTags.includes(entry.tag))
+					.reduce<Map<string, string[]>>((acc, entry) => {
+						const prevUserIds = acc.get(entry.tag) ?? [];
+						const userIds = [...prevUserIds, entry.userId];
+						return acc.set(entry.tag, userIds);
+					}, new Map()),
+			].map(([name, users]) => ({ name, users }));
 
-			for (const note of tagNotes) {
-				for (const tag of note.tags) {
-					if (hiddenTags.includes(tag)) continue;
-
-					const x = tags.find((x) => x.name === tag);
-					if (x) {
-						if (!x.users.includes(note.userId)) {
-							x.users.push(note.userId);
-						}
-					} else {
-						tags.push({
-							name: tag,
-							users: [note.userId],
-						});
-					}
-				}
-			}
-
-			// タグを人気順に並べ替え
-			const hots = tags
+			const hotTags = tags
 				.sort((a, b) => b.users.length - a.users.length)
-				.map((tag) => tag.name)
-				.slice(0, max);
+				.slice(0, max)
+				.map((tag) => tag.name);
 
 			//#region 2(または3)で話題と判定されたタグそれぞれについて過去の投稿数グラフを取得する
-			const countPromises: Promise<number[]>[] = [];
-
 			const range = 20;
+			const interval = 1000 * 60 * 10; // 10分
 
-			// 10分
-			const interval = 1000 * 60 * 10;
-
-			for (let i = 0; i < range; i++) {
-				countPromises.push(
-					Promise.all(
-						hots.map(
-							async (tag) =>
-								(
-									await this.prismaService.client.note.aggregate({
-										_count: { userId: true },
-										where: {
-											tags: { has: tag },
-											createdAt: {
-												lt: new Date(now.getTime() - interval * (i + 0)),
-												gt: new Date(now.getTime() - interval * (i + 1)),
-											},
-										},
-									})
-								)._count.userId,
-						),
-					),
-				);
-			}
-
-			const countsLog = await Promise.all(countPromises);
-			//#endregion
-
-			const totalCounts = await Promise.all(
-				hots.map(
-					async (tag) =>
-						(
-							await this.prismaService.client.note.aggregate({
+			const countsLog = await Promise.all(
+				Array.from({ length: range }, async (_, i) => {
+					return await Promise.all(
+						hotTags.map(async (tag) => {
+							const result = await this.prismaService.client.note.aggregate({
 								_count: { userId: true },
 								where: {
 									tags: { has: tag },
-									createdAt: { gt: new Date(now.getTime() - rangeA) },
+									createdAt: {
+										lt: new Date(now.getTime() - interval * (i + 0)),
+										gt: new Date(now.getTime() - interval * (i + 1)),
+									},
 								},
-							})
-						)._count.userId,
-				),
+							});
+
+							return result._count.userId;
+						}),
+					);
+				}),
+			);
+			//#endregion
+
+			const totalCounts = await Promise.all(
+				hotTags.map(async (tag) => {
+					const result = await this.prismaService.client.note.aggregate({
+						_count: { userId: true },
+						where: {
+							tags: { has: tag },
+							createdAt: { gt: new Date(now.getTime() - rangeA) },
+						},
+					});
+
+					return result._count.userId;
+				}),
 			);
 
-			const stats = hots.map((tag, i) => ({
+			const stats = hotTags.map((tag, i) => ({
 				tag,
 				chart: countsLog.map((counts) => counts[i]),
 				usersCount: totalCounts[i],
 			}));
 
-			return stats satisfies z.infer<typeof res>;
+			return stats;
 		});
 	}
 }
