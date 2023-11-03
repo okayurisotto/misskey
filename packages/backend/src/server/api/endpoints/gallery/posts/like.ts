8269dfa6e21592bfe0cf2b871a67c752d5c137fa
okayurisotto <okayurisotto@proton.me>
2023-11-04 01:57:19 +0900

    backend: refactor `/gallery/posts/like`

--- packages/backend/src/server/api/endpoints/gallery/posts/like.ts
+++ packages/backend/src/server/api/endpoints/gallery/posts/like.ts
@@ -1,18 +1,15 @@
 import { z } from 'zod';
 import { Injectable } from '@nestjs/common';
-import { noSuchPost_, yourPost, alreadyLiked_ } from '@/server/api/errors.js';
 import { Endpoint } from '@/server/api/abstract-endpoint.js';
 import { IdService } from '@/core/IdService.js';
 import { MisskeyIdSchema } from '@/models/zod/misc.js';
 import { PrismaService } from '@/core/PrismaService.js';
-import { ApiError } from '../../../error.js';
 
 export const meta = {
 	tags: ['gallery'],
 	requireCredential: true,
 	prohibitMoved: true,
 	kind: 'write:gallery-likes',
-	errors: {noSuchPost:noSuchPost_,yourPost:yourPost,alreadyLiked:alreadyLiked_},
 } as const;
 
 export const paramDef = z.object({
@@ -31,45 +28,22 @@ export default class extends Endpoint<
 		private readonly prismaService: PrismaService,
 	) {
 		super(meta, paramDef, async (ps, me) => {
-			const post = await this.prismaService.client.gallery_post.findUnique({
-				where: {
-					id: ps.postId,
-				},
-			});
-			if (post == null) {
-				throw new ApiError(meta.errors.noSuchPost);
-			}
-
-			if (post.userId === me.id) {
-				throw new ApiError(meta.errors.yourPost);
-			}
-
-			// if already liked
-			const exist =
-				(await this.prismaService.client.gallery_like.count({
-					where: {
-						postId: post.id,
-						userId: me.id,
-					},
-					take: 1,
-				})) > 0;
-
-			if (exist) {
-				throw new ApiError(meta.errors.alreadyLiked);
-			}
-
-			// Create like
 			await this.prismaService.client.gallery_like.create({
 				data: {
 					id: this.idService.genId(),
 					createdAt: new Date(),
-					postId: post.id,
-					userId: me.id,
+					user: { connect: { id: me.id } },
+					gallery_post: {
+						connect: {
+							id: ps.postId,
+							userId: { not: me.id },
+						},
+					},
 				},
 			});
 
 			await this.prismaService.client.gallery_post.update({
-				where: { id: post.id },
+				where: { id: ps.postId },
 				data: { likedCount: { increment: 1 } },
 			});
 		});
