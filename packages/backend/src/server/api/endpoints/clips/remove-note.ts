e00c50e55fa2debab417cedc0e220642075eeb7a
okayurisotto <okayurisotto@proton.me>
2023-11-04 02:42:54 +0900

    backend: refactor `/clips/remove-note`

--- packages/backend/src/server/api/endpoints/clips/remove-note.ts
+++ packages/backend/src/server/api/endpoints/clips/remove-note.ts
@@ -1,18 +1,14 @@
 import { z } from 'zod';
 import { Injectable } from '@nestjs/common';
-import { noSuchClip____, noSuchNote__ } from '@/server/api/errors.js';
 import { Endpoint } from '@/server/api/abstract-endpoint.js';
-import { GetterService } from '@/server/api/GetterService.js';
 import { MisskeyIdSchema } from '@/models/zod/misc.js';
 import { PrismaService } from '@/core/PrismaService.js';
-import { ApiError } from '../../error.js';
 
 export const meta = {
 	tags: ['account', 'notes', 'clips'],
 	requireCredential: true,
 	prohibitMoved: true,
 	kind: 'write:account',
-	errors: {noSuchClip:noSuchClip____,noSuchNote:noSuchNote__},
 } as const;
 
 export const paramDef = z.object({
@@ -27,35 +23,15 @@ export default class extends Endpoint<
 	typeof paramDef,
 	z.ZodType<void>
 > {
-	constructor(
-		private readonly getterService: GetterService,
-		private readonly prismaService: PrismaService,
-	) {
+	constructor(private readonly prismaService: PrismaService) {
 		super(meta, paramDef, async (ps, me) => {
-			const clip = await this.prismaService.client.clip.findUnique({
-				where: {
-					id: ps.clipId,
-					userId: me.id,
-				},
-			});
-
-			if (clip == null) {
-				throw new ApiError(meta.errors.noSuchClip);
-			}
-
-			const note = await this.getterService.getNote(ps.noteId).catch((err) => {
-				if (err.id === '9725d0ce-ba28-4dde-95a7-2cbb2c15de24') {
-					throw new ApiError(meta.errors.noSuchNote);
-				}
-				throw err;
-			});
-
 			await this.prismaService.client.clip_note.delete({
 				where: {
 					noteId_clipId: {
-						noteId: note.id,
-						clipId: clip.id,
+						noteId: ps.noteId,
+						clipId: ps.clipId,
 					},
+					clip: { userId: me.id },
 				},
 			});
 		});
