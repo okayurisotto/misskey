8993f387d83263f98bd993d1e1d5e34587395ec9
okayurisotto <okayurisotto@proton.me>
2023-11-04 01:57:57 +0900

    backend: refactor `/gallery/posts/unlike`

--- packages/backend/src/server/api/endpoints/gallery/posts/unlike.ts
+++ packages/backend/src/server/api/endpoints/gallery/posts/unlike.ts
@@ -1,17 +1,14 @@
 import { z } from 'zod';
 import { Injectable } from '@nestjs/common';
-import { noSuchPost___, notLiked_ } from '@/server/api/errors.js';
 import { Endpoint } from '@/server/api/abstract-endpoint.js';
 import { MisskeyIdSchema } from '@/models/zod/misc.js';
 import { PrismaService } from '@/core/PrismaService.js';
-import { ApiError } from '../../../error.js';
 
 export const meta = {
 	tags: ['gallery'],
 	requireCredential: true,
 	prohibitMoved: true,
 	kind: 'write:gallery-likes',
-	errors: {noSuchPost:noSuchPost___,notLiked:notLiked_},
 } as const;
 
 export const paramDef = z.object({
@@ -27,35 +24,12 @@ export default class extends Endpoint<
 > {
 	constructor(private readonly prismaService: PrismaService) {
 		super(meta, paramDef, async (ps, me) => {
-			const post = await this.prismaService.client.gallery_post.findUnique({
-				where: {
-					id: ps.postId,
-				},
-			});
-			if (post == null) {
-				throw new ApiError(meta.errors.noSuchPost);
-			}
-
-			const exist = await this.prismaService.client.gallery_like.findUnique({
-				where: {
-					userId_postId: {
-						postId: post.id,
-						userId: me.id,
-					},
-				},
-			});
-
-			if (exist == null) {
-				throw new ApiError(meta.errors.notLiked);
-			}
-
-			// Delete like
 			await this.prismaService.client.gallery_like.delete({
-				where: { id: exist.id },
+				where: { id: ps.postId, userId: me.id },
 			});
 
 			await this.prismaService.client.gallery_post.update({
-				where: { id: post.id },
+				where: { id: ps.postId },
 				data: { likedCount: { decrement: 1 } },
 			});
 		});
