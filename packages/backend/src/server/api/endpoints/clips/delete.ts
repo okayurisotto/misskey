121421372abaac19b57c13f0dde6c2a764e54a58
okayurisotto <okayurisotto@proton.me>
2023-11-04 02:35:45 +0900

    backend: refactor `/clips/delete`

--- packages/backend/src/server/api/endpoints/clips/delete.ts
+++ packages/backend/src/server/api/endpoints/clips/delete.ts
@@ -1,16 +1,13 @@
 import { z } from 'zod';
 import { Injectable } from '@nestjs/common';
-import { noSuchClip_ } from '@/server/api/errors.js';
 import { Endpoint } from '@/server/api/abstract-endpoint.js';
 import { MisskeyIdSchema } from '@/models/zod/misc.js';
 import { PrismaService } from '@/core/PrismaService.js';
-import { ApiError } from '../../error.js';
 
 export const meta = {
 	tags: ['clips'],
 	requireCredential: true,
 	kind: 'write:account',
-	errors: {noSuchClip:noSuchClip_},
 } as const;
 
 export const paramDef = z.object({
@@ -26,18 +23,9 @@ export default class extends Endpoint<
 > {
 	constructor(private readonly prismaService: PrismaService) {
 		super(meta, paramDef, async (ps, me) => {
-			const clip = await this.prismaService.client.clip.findUnique({
-				where: {
-					id: ps.clipId,
-					userId: me.id,
-				},
+			await this.prismaService.client.clip.delete({
+				where: { id: ps.clipId, userId: me.id },
 			});
-
-			if (clip == null) {
-				throw new ApiError(meta.errors.noSuchClip);
-			}
-
-			await this.prismaService.client.clip.delete({ where: { id: clip.id } });
 		});
 	}
 }
