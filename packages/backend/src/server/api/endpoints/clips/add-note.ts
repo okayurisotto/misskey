d89306613c895d76f1389571934c782ce90ba107
okayurisotto <okayurisotto@proton.me>
2023-11-04 02:35:26 +0900

    backend: refactor `/clips/add-note`

--- packages/backend/src/server/api/endpoints/clips/add-note.ts
+++ packages/backend/src/server/api/endpoints/clips/add-note.ts
@@ -1,10 +1,9 @@
 import { z } from 'zod';
 import { Injectable } from '@nestjs/common';
 import ms from 'ms';
-import { noSuchClip, noSuchNote_, alreadyClipped, tooManyClipNotes } from '@/server/api/errors.js';
+import { tooManyClipNotes } from '@/server/api/errors.js';
 import { Endpoint } from '@/server/api/abstract-endpoint.js';
 import { IdService } from '@/core/IdService.js';
-import { GetterService } from '@/server/api/GetterService.js';
 import { RoleService } from '@/core/RoleService.js';
 import { MisskeyIdSchema } from '@/models/zod/misc.js';
 import { PrismaService } from '@/core/PrismaService.js';
@@ -19,7 +18,7 @@ export const meta = {
 		duration: ms('1hour'),
 		max: 20,
 	},
-	errors: {noSuchClip:noSuchClip,noSuchNote:noSuchNote_,alreadyClipped:alreadyClipped,tooManyClipNotes:tooManyClipNotes},
+	errors: { tooManyClipNotes: tooManyClipNotes },
 } as const;
 
 export const paramDef = z.object({
@@ -37,61 +36,30 @@ export default class extends Endpoint<
 	constructor(
 		private readonly idService: IdService,
 		private readonly roleService: RoleService,
-		private readonly getterService: GetterService,
 		private readonly prismaService: PrismaService,
 	) {
 		super(meta, paramDef, async (ps, me) => {
-			const clip = await this.prismaService.client.clip.findUnique({
-				where: {
-					id: ps.clipId,
-					userId: me.id,
-				},
-			});
+			const [currentCount, policies] = await Promise.all([
+				this.prismaService.client.clip_note.count({
+					where: { clipId: ps.clipId },
+				}),
+				this.roleService.getUserPolicies(me.id),
+			]);
 
-			if (clip == null) {
-				throw new ApiError(meta.errors.noSuchClip);
-			}
-
-			const note = await this.getterService.getNote(ps.noteId).catch((e) => {
-				if (e.id === '9725d0ce-ba28-4dde-95a7-2cbb2c15de24') {
-					throw new ApiError(meta.errors.noSuchNote);
-				}
-				throw e;
-			});
-
-			const exist =
-				(await this.prismaService.client.clip_note.count({
-					where: {
-						noteId: note.id,
-						clipId: clip.id,
-					},
-					take: 1,
-				})) > 0;
-
-			if (exist) {
-				throw new ApiError(meta.errors.alreadyClipped);
-			}
-
-			const currentCount = await this.prismaService.client.clip_note.count({
-				where: { clipId: clip.id },
-			});
-			if (
-				currentCount >
-				(await this.roleService.getUserPolicies(me.id)).noteEachClipsLimit
-			) {
+			if (currentCount > policies.noteEachClipsLimit) {
 				throw new ApiError(meta.errors.tooManyClipNotes);
 			}
 
 			await this.prismaService.client.clip_note.create({
 				data: {
 					id: this.idService.genId(),
-					noteId: note.id,
-					clipId: clip.id,
+					noteId: ps.noteId,
+					clipId: ps.clipId,
 				},
 			});
 
 			await this.prismaService.client.clip.update({
-				where: { id: clip.id },
+				where: { id: ps.clipId },
 				data: { lastClippedAt: new Date() },
 			});
 		});
