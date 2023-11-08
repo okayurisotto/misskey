53dc13a1de391a5bb2389ff41cf4405cf2db9abe
okayurisotto <okayurisotto@proton.me>
2023-11-08 13:55:17 +0900

    backend: refactor `AggregateRetentionProcessorService.ts`
    - `Set.has`で負荷軽減
    - `Promise.all`で解決をきちんと待つように
    
    - 最大31回の`UPDATE`は、データベース設計が悪いため避けられない

--- packages/backend/src/queue/processors/AggregateRetentionProcessorService.ts
+++ packages/backend/src/queue/processors/AggregateRetentionProcessorService.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { z } from 'zod';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library.js';
 import type Logger from '@/logger.js';
 import { bindThis } from '@/decorators.js';
 import { deepClone } from '@/misc/clone.js';
 import { IdService } from '@/core/IdService.js';
-import { isDuplicateKeyValueError } from '@/misc/is-duplicate-key-value-error.js';
 import { PrismaService } from '@/core/PrismaService.js';
 import { QueueLoggerService } from '../QueueLoggerService.js';
 
@@ -17,7 +17,9 @@ export class AggregateRetentionProcessorService {
 		private readonly queueLoggerService: QueueLoggerService,
 		private readonly prismaService: PrismaService,
 	) {
-		this.logger = this.queueLoggerService.logger.createSubLogger('aggregate-retention');
+		this.logger = this.queueLoggerService.logger.createSubLogger(
+			'aggregate-retention',
+		);
 	}
 
 	@bindThis
@@ -25,23 +27,28 @@ export class AggregateRetentionProcessorService {
 		this.logger.info('Aggregating retention...');
 
 		const now = new Date();
-		const dateKey = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`;
+		const dateKey = [
+			now.getFullYear(),
+			now.getMonth() + 1,
+			now.getDate(),
+		].join('-');
 
-		// 過去(だいたい)30日分のレコードを取得
-		const pastRecords = await this.prismaService.client.retention_aggregation.findMany({
-			where: {
-				createdAt: { gt: new Date(Date.now() - (1000 * 60 * 60 * 24 * 31)) },
-			},
-		});
+		/** 過去31日分のレコード */
+		const pastRecords =
+			await this.prismaService.client.retention_aggregation.findMany({
+				where: {
+					createdAt: { gt: new Date(+now - 1000 * 60 * 60 * 24 * 31) },
+				},
+			});
 
-		// 今日登録したユーザーを全て取得
 		const targetUsers = await this.prismaService.client.user.findMany({
 			where: {
 				host: null,
-				createdAt: { gt: new Date(Date.now() - (1000 * 60 * 60 * 24)) },
+				createdAt: { gt: new Date(+now - 1000 * 60 * 60 * 24) },
 			},
 		});
-		const targetUserIds = targetUsers.map(u => u.id);
+		/** 今日登録したユーザーのID */
+		const targetUserIds = targetUsers.map((u) => u.id);
 
 		try {
 			await this.prismaService.client.retention_aggregation.create({
@@ -55,36 +62,44 @@ export class AggregateRetentionProcessorService {
 				},
 			});
 		} catch (err) {
-			if (isDuplicateKeyValueError(err)) {
-				this.logger.succ('Skip because it has already been processed by another worker.');
-				return;
+			if (err instanceof PrismaClientKnownRequestError) {
+				if (err.code === 'P2002') {
+					this.logger.succ(
+						'Skip because it has already been processed by another worker.',
+					);
+					return;
+				}
 			}
 			throw err;
 		}
 
-		// 今日活動したユーザーを全て取得
 		const activeUsers = await this.prismaService.client.user.findMany({
 			where: {
 				host: null,
-				lastActiveDate: { gt: new Date(Date.now() - (1000 * 60 * 60 * 24)) },
+				lastActiveDate: { gt: new Date(+now - 1000 * 60 * 60 * 24) },
 			},
 		});
-		const activeUsersIds = activeUsers.map(u => u.id);
+		/** 今日活動したユーザーのID */
+		const activeUsersIds = new Set(activeUsers.map((u) => u.id));
 
-		for (const record of pastRecords) {
-			const retention = record.userIds.filter(id => activeUsersIds.includes(id)).length;
+		await Promise.all(
+			pastRecords.map(async (record) => {
+				const retention = record.userIds.filter((id) =>
+					activeUsersIds.has(id),
+				).length;
 
-			this.prismaService.client.retention_aggregation.update({
-				where: { id: record.id },
-				data: {
-					updatedAt: now,
+				await this.prismaService.client.retention_aggregation.update({
+					where: { id: record.id },
 					data: {
-						...deepClone(z.record(z.string(), z.number()).parse(record.data)),
-						[dateKey]: retention,
+						updatedAt: now,
+						data: {
+							...deepClone(z.record(z.string(), z.number()).parse(record.data)),
+							[dateKey]: retention,
+						},
 					},
-				},
-			});
-		}
+				});
+			}),
+		);
 
 		this.logger.succ('Retention aggregated.');
 	}
