577d9e91a106e3443ab70b28cebf3598973f5c7e
okayurisotto <okayurisotto@proton.me>
2023-11-08 16:16:54 +0900

    backend: refactor `reset-db.ts`

--- packages/backend/src/misc/reset-db.ts
+++ packages/backend/src/misc/reset-db.ts
@@ -2,27 +2,29 @@ import { setTimeout } from 'node:timers/promises';
 import { z } from 'zod';
 import type { DataSource } from 'typeorm';
 
-export const resetDb = async (db: DataSource): Promise<void> => {
-	const reset = async (): Promise<void> => {
-		const tables_ = await db.query(`
-			SELECT relname AS "table"
-				FROM pg_class C LEFT JOIN pg_namespace N ON (N.oid = C.relnamespace)
-				WHERE nspname NOT IN ('pg_catalog', 'information_schema')
-					AND C.relkind = 'r'
-					AND nspname !~ '^pg_toast';
-		`);
-		const tables = z.array(z.object({ table: z.string() })).parse(tables_);
+const reset = async (db: DataSource): Promise<void> => {
+	const tables_: unknown = await db.query(`
+		SELECT relname AS "table"
+			FROM pg_class C LEFT JOIN pg_namespace N ON (N.oid = C.relnamespace)
+			WHERE nspname NOT IN ('pg_catalog', 'information_schema')
+				AND C.relkind = 'r'
+				AND nspname !~ '^pg_toast';
+	`);
+	const tables = z.array(z.object({ table: z.string() })).parse(tables_);
 
-		for (const { table } of tables) {
+	await Promise.all(
+		tables.map(async ({ table }) => {
 			await db.query(`DELETE FROM "${table}" CASCADE`);
-		}
-	};
+		}),
+	);
+};
 
+export const resetDb = async (db: DataSource): Promise<void> => {
 	const maxRetries = 3;
 
 	for (let count = 1; count <= maxRetries; count++) {
 		try {
-			await reset();
+			await reset(db);
 		} catch (e) {
 			if (count === maxRetries) {
 				throw e;
