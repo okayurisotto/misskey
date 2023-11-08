08dbe57e2c70d0b041493731b9c1092e60f94a93
okayurisotto <okayurisotto@proton.me>
2023-11-09 01:30:40 +0900

    backend: refactor `DeleteDriveFilesProcessorService.ts`

--- packages/backend/src/queue/processors/DeleteDriveFilesProcessorService.ts
+++ packages/backend/src/queue/processors/DeleteDriveFilesProcessorService.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { drive_file } from '@prisma/client';
 import type Logger from '@/logger.js';
 import { DriveService } from '@/core/DriveService.js';
 import { bindThis } from '@/decorators.js';
@@ -17,50 +16,34 @@ export class DeleteDriveFilesProcessorService {
 		private readonly queueLoggerService: QueueLoggerService,
 		private readonly prismaService: PrismaService,
 	) {
-		this.logger = this.queueLoggerService.logger.createSubLogger('delete-drive-files');
+		this.logger =
+			this.queueLoggerService.logger.createSubLogger('delete-drive-files');
 	}
 
 	@bindThis
 	public async process(job: Bull.Job<DbJobDataWithUser>): Promise<void> {
 		this.logger.info(`Deleting drive files of ${job.data.user.id} ...`);
 
-		const user = await this.prismaService.client.user.findUnique({ where: { id: job.data.user.id } });
-		if (user == null) {
-			return;
-		}
+		const user = await this.prismaService.client.user.findUnique({
+			where: { id: job.data.user.id },
+			include: { drive_file_drive_file_userIdTouser: true },
+		});
+		if (user == null) return;
 
-		let deletedCount = 0;
-		let cursor: drive_file['id'] | null = null;
+		const files = user.drive_file_drive_file_userIdTouser;
+		const total = files.length;
+		let count = 0;
 
-		while (true) {
-			const files: drive_file[] = await this.prismaService.client.drive_file.findMany({
-				where: {
-					userId: user.id,
-					...(cursor ? { id: { gt: cursor } } : {}),
-				},
-				take: 100,
-				orderBy: { id: 'asc' },
-			});
-
-			if (files.length === 0) {
-				job.updateProgress(100);
-				break;
-			}
-
-			cursor = files.at(-1)?.id ?? null;
-
-			for (const file of files) {
+		await Promise.all(
+			files.map(async (file) => {
 				await this.driveService.deleteFileSync(file);
-				deletedCount++;
-			}
+				count++;
+				await job.updateProgress(count / total);
+			}),
+		);
 
-			const total = await this.prismaService.client.drive_file.count({
-				where: { userId: user.id },
-			});
-
-			job.updateProgress(deletedCount / total);
-		}
-
-		this.logger.succ(`All drive files (${deletedCount}) of ${user.id} has been deleted.`);
+		this.logger.succ(
+			`All drive files (${count}) of ${user.id} has been deleted.`,
+		);
 	}
 }
