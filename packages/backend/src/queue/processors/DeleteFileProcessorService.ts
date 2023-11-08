be90053d30af4192d098f9e2ce41dbd998fd4fce
okayurisotto <okayurisotto@proton.me>
2023-11-09 01:36:13 +0900

    backend: refactor `DeleteFileProcessorService.ts`

--- packages/backend/src/queue/processors/DeleteFileProcessorService.ts
+++ packages/backend/src/queue/processors/DeleteFileProcessorService.ts
@@ -1,23 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { DriveService } from '@/core/DriveService.js';
 import { bindThis } from '@/decorators.js';
-import { QueueLoggerService } from '../QueueLoggerService.js';
 import type * as Bull from 'bullmq';
 import type { ObjectStorageFileJobData } from '../types.js';
 
 @Injectable()
 export class DeleteFileProcessorService {
-	constructor(
-		private readonly driveService: DriveService,
-		private readonly queueLoggerService: QueueLoggerService,
-	) {}
+	constructor(private readonly driveService: DriveService) {}
 
 	@bindThis
-	public async process(job: Bull.Job<ObjectStorageFileJobData>): Promise<string> {
-		const key: string = job.data.key;
-
-		await this.driveService.deleteObjectStorageFile(key);
-
+	public async process(
+		job: Bull.Job<ObjectStorageFileJobData>,
+	): Promise<string> {
+		await this.driveService.deleteObjectStorageFile(job.data.key);
 		return 'Success';
 	}
 }
