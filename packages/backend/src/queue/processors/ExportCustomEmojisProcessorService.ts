cdd08074afb3bc357c0f06e827d3605181b5ede3
okayurisotto <okayurisotto@proton.me>
2023-11-09 02:29:53 +0900

    backend: refactor `ExportCustomEmojisProcessorService.ts`
    カスタム絵文字のダウンロードを`Promise.all`したり、JSONを手動で組み立てたりするのをやめた。

--- packages/backend/src/queue/processors/ExportCustomEmojisProcessorService.ts
+++ packages/backend/src/queue/processors/ExportCustomEmojisProcessorService.ts
@@ -3,6 +3,7 @@ import { Inject, Injectable } from '@nestjs/common';
 import { format as dateFormat } from 'date-fns';
 import mime from 'mime-types';
 import archiver from 'archiver';
+import { z } from 'zod';
 import { DI } from '@/di-symbols.js';
 import type { Config } from '@/config.js';
 import type Logger from '@/logger.js';
@@ -27,101 +28,125 @@ export class ExportCustomEmojisProcessorService {
 		private readonly queueLoggerService: QueueLoggerService,
 		private readonly prismaService: PrismaService,
 	) {
-		this.logger = this.queueLoggerService.logger.createSubLogger('export-custom-emojis');
+		this.logger = this.queueLoggerService.logger.createSubLogger(
+			'export-custom-emojis',
+		);
 	}
 
 	@bindThis
 	public async process(job: Bull.Job): Promise<void> {
 		this.logger.info('Exporting custom emojis ...');
 
-		const user = await this.prismaService.client.user.findUnique({ where: { id: job.data.user.id } });
-		if (user == null) {
-			return;
-		}
+		const job_ = z
+			.object({ data: z.object({ user: z.object({ id: z.string() }) }) })
+			.parse(job);
+
+		const user = await this.prismaService.client.user.findUnique({
+			where: { id: job_.data.user.id },
+		});
+		if (user === null) return;
 
 		const [path, cleanup] = await createTempDir();
 
 		this.logger.info(`Temp dir is ${path}`);
 
-		const metaPath = path + '/meta.json';
-
-		fs.writeFileSync(metaPath, '', 'utf-8');
-
-		const metaStream = fs.createWriteStream(metaPath, { flags: 'a' });
-
-		const writeMeta = (text: string): Promise<void> => {
-			return new Promise<void>((res, rej) => {
-				metaStream.write(text, err => {
-					if (err) {
-						this.logger.error(err);
-						rej(err);
-					} else {
-						res();
-					}
-				});
-			});
-		};
-
-		await writeMeta(`{"metaVersion":2,"host":"${this.config.host}","exportedAt":"${new Date().toString()}","emojis":[`);
+		// Download all custom emojis
 
 		const customEmojis = await this.prismaService.client.emoji.findMany({
 			where: { host: null },
 			orderBy: { id: 'asc' },
 		});
 
-		for (const emoji of customEmojis) {
-			if (!/^[a-zA-Z0-9_]+$/.test(emoji.name)) {
-				this.logger.error(`invalid emoji name: ${emoji.name}`);
-				continue;
-			}
-			const ext = mime.extension(emoji.type ?? 'image/png');
-			const fileName = emoji.name + (ext ? '.' + ext : '');
-			const emojiPath = path + '/' + fileName;
-			fs.writeFileSync(emojiPath, '', 'binary');
-			let downloaded = false;
+		const customEmoji_ = await Promise.all(
+			customEmojis
+				.filter((emoji) => {
+					if (!/^[a-zA-Z0-9_]+$/.test(emoji.name)) {
+						this.logger.error(`invalid emoji name: ${emoji.name}`);
+						return false;
+					}
+					return true;
+				})
+				.map((emoji) => {
+					const ext = mime.extension(emoji.type ?? 'image/png');
+					const fileName = emoji.name + (ext ? '.' + ext : '');
+					const emojiPath = path + '/' + fileName;
+					return { emoji, fileName, emojiPath };
+				})
+				.map(async ({ emoji, emojiPath, fileName }) => {
+					fs.writeFileSync(emojiPath, '', 'binary');
+					let downloaded = false;
 
-			try {
-				await this.downloadService.downloadUrl(emoji.originalUrl, emojiPath);
-				downloaded = true;
-			} catch (e) { // TODO: 何度か再試行
-				this.logger.error(e instanceof Error ? e : new Error(e as string));
-			}
+					try {
+						await this.downloadService.downloadUrl(
+							emoji.originalUrl,
+							emojiPath,
+						);
+						downloaded = true;
+					} catch (e) {
+						// TODO: 何度か再試行
 
-			if (!downloaded) {
-				fs.unlinkSync(emojiPath);
-			}
+						if (e instanceof Error) {
+							this.logger.error(e);
+						} else if (typeof e === 'string') {
+							this.logger.error(new Error(e));
+						} else {
+							// TODO
+						}
+					} finally {
+						if (!downloaded) {
+							fs.unlinkSync(emojiPath);
+						}
+					}
 
-			const content = JSON.stringify({
+					return { emoji, fileName, downloaded };
+				}),
+		);
+
+		// Write meta.json
+
+		const metaPath = path + '/meta.json';
+
+		const metaContent = customEmoji_.map(({ emoji, fileName, downloaded }) => {
+			return {
+				emoji: emoji,
 				fileName: fileName,
 				downloaded: downloaded,
-				emoji: emoji,
-			});
-			const isFirst = customEmojis.indexOf(emoji) === 0;
+			};
+		});
 
-			await writeMeta(isFirst ? content : ',\n' + content);
-		}
+		const meta = {
+			metaVersion: 2,
+			host: this.config.host,
+			exportedAt: new Date().toString(),
+			emojis: metaContent,
+		};
 
-		await writeMeta(']}');
-
-		metaStream.end();
+		fs.writeFileSync(metaPath, JSON.stringify(meta));
 
 		// Create archive
+
 		const [archivePath, archiveCleanup] = await createTemp();
 		const archiveStream = fs.createWriteStream(archivePath);
-		const archive = archiver('zip', {
-			zlib: { level: 0 },
-		});
+		const archive = archiver('zip', { zlib: { level: 0 } });
 
 		archive.pipe(archiveStream);
 		archive.directory(path, false);
-		archive.finalize();
+		await archive.finalize();
 
 		await new Promise<void>((resolve) => {
 			archiveStream.on('close', async () => {
 				this.logger.succ(`Exported to: ${archivePath}`);
 
-				const fileName = 'custom-emojis-' + dateFormat(new Date(), 'yyyy-MM-dd-HH-mm-ss') + '.zip';
-				const driveFile = await this.driveService.addFile({ user, path: archivePath, name: fileName, force: true });
+				const fileName =
+					'custom-emojis-' +
+					dateFormat(new Date(), 'yyyy-MM-dd-HH-mm-ss') +
+					'.zip';
+				const driveFile = await this.driveService.addFile({
+					user,
+					path: archivePath,
+					name: fileName,
+					force: true,
+				});
 
 				this.logger.succ(`Exported to: ${driveFile.id}`);
 				cleanup();
