8b323b8305d218987f13f8965dd946aecbdf6212
okayurisotto <okayurisotto@proton.me>
2023-11-09 03:15:55 +0900

    backend: refactor `ExportMutingProcessorService.ts`

--- packages/backend/src/queue/processors/ExportMutingProcessorService.ts
+++ packages/backend/src/queue/processors/ExportMutingProcessorService.ts
@@ -10,7 +10,6 @@ import { PrismaService } from '@/core/PrismaService.js';
 import { QueueLoggerService } from '../QueueLoggerService.js';
 import type * as Bull from 'bullmq';
 import type { DbJobDataWithUser } from '../types.js';
-import type { muting } from '@prisma/client';
 
 @Injectable()
 export class ExportMutingProcessorService {
@@ -22,81 +21,50 @@ export class ExportMutingProcessorService {
 		private readonly queueLoggerService: QueueLoggerService,
 		private readonly prismaService: PrismaService,
 	) {
-		this.logger = this.queueLoggerService.logger.createSubLogger('export-muting');
+		this.logger =
+			this.queueLoggerService.logger.createSubLogger('export-muting');
 	}
 
 	@bindThis
 	public async process(job: Bull.Job<DbJobDataWithUser>): Promise<void> {
 		this.logger.info(`Exporting muting of ${job.data.user.id} ...`);
 
-		const user = await this.prismaService.client.user.findUnique({ where: { id: job.data.user.id } });
-		if (user == null) {
-			return;
-		}
+		const user = await this.prismaService.client.user.findUnique({
+			where: { id: job.data.user.id },
+			include: {
+				muting_muting_muterIdTouser: {
+					include: { user_muting_muteeIdTouser: true },
+				},
+			},
+		});
+		if (user === null) return;
+
+		const content = user.muting_muting_muterIdTouser
+			.map((mute) => {
+				const user = mute.user_muting_muteeIdTouser;
+				return this.utilityService.getFullApAccount(user.username, user.host);
+			})
+			.map((entry) => entry + '\n')
+			.join('');
 
 		// Create temp file
 		const [path, cleanup] = await createTemp();
-
 		this.logger.info(`Temp file is ${path}`);
 
 		try {
-			const stream = fs.createWriteStream(path, { flags: 'a' });
+			fs.writeFileSync(path, content);
 
-			let exportedCount = 0;
-			let cursor: muting['id'] | null = null;
-
-			while (true) {
-				const mutes: muting[] = await this.prismaService.client.muting.findMany({
-					where: {
-						muterId: user.id,
-						expiresAt: null,
-						...(cursor ? { id: { gt: cursor } } : {}),
-					},
-					take: 100,
-					orderBy: { id: 'asc' },
-				});
-
-				if (mutes.length === 0) {
-					job.updateProgress(100);
-					break;
-				}
-
-				cursor = mutes.at(-1)?.id ?? null;
-
-				for (const mute of mutes) {
-					const u = await this.prismaService.client.user.findUnique({ where: { id: mute.muteeId } });
-					if (u == null) {
-						exportedCount++; continue;
-					}
-
-					const content = this.utilityService.getFullApAccount(u.username, u.host);
-					await new Promise<void>((res, rej) => {
-						stream.write(content + '\n', err => {
-							if (err) {
-								this.logger.error(err);
-								rej(err);
-							} else {
-								res();
-							}
-						});
-					});
-					exportedCount++;
-				}
-
-				const total = await this.prismaService.client.muting.count({
-					where: {
-						muterId: user.id,
-					},
-				});
-
-				job.updateProgress(exportedCount / total);
-			}
-
-			stream.end();
 			this.logger.succ(`Exported to: ${path}`);
 
-			const fileName = 'mute-' + dateFormat(new Date(), 'yyyy-MM-dd-HH-mm-ss') + '.csv';
-			const driveFile = await this.driveService.addFile({ user, path, name: fileName, force: true, ext: 'csv' });
+			const fileName =
+				'mute-' + dateFormat(new Date(), 'yyyy-MM-dd-HH-mm-ss') + '.csv';
+			const driveFile = await this.driveService.addFile({
+				user,
+				path,
+				name: fileName,
+				force: true,
+				ext: 'csv',
+			});
 
 			this.logger.succ(`Exported to: ${driveFile.id}`);
 		} finally {
