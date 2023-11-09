0d31df6280f7a353e8676e8ac39c5ec2309814b8
okayurisotto <okayurisotto@proton.me>
2023-11-09 09:50:08 +0900

    backend: refactor `ExportNotesProcessorService.ts`

--- packages/backend/src/queue/processors/ExportNotesProcessorService.ts
+++ packages/backend/src/queue/processors/ExportNotesProcessorService.ts
@@ -1,18 +1,17 @@
 import * as fs from 'node:fs';
 import { Injectable } from '@nestjs/common';
 import { format as dateFormat } from 'date-fns';
+import { pick } from 'omick';
 import type Logger from '@/logger.js';
 import { DriveService } from '@/core/DriveService.js';
 import { createTemp } from '@/misc/create-temp.js';
 import { bindThis } from '@/decorators.js';
 import { DriveFileEntityService } from '@/core/entities/DriveFileEntityService.js';
-import type { DriveFileSchema } from '@/models/zod/DriveFileSchema.js';
 import { PrismaService } from '@/core/PrismaService.js';
+import { unique } from '@/misc/prelude/array.js';
 import { QueueLoggerService } from '../QueueLoggerService.js';
 import type * as Bull from 'bullmq';
 import type { DbJobDataWithUser } from '../types.js';
-import type { z } from 'zod';
-import type { note, poll } from '@prisma/client';
 
 @Injectable()
 export class ExportNotesProcessorService {
@@ -24,109 +23,60 @@ export class ExportNotesProcessorService {
 		private readonly driveFileEntityService: DriveFileEntityService,
 		private readonly prismaService: PrismaService,
 	) {
-		this.logger = this.queueLoggerService.logger.createSubLogger('export-notes');
+		this.logger =
+			this.queueLoggerService.logger.createSubLogger('export-notes');
 	}
 
 	@bindThis
 	public async process(job: Bull.Job<DbJobDataWithUser>): Promise<void> {
 		this.logger.info(`Exporting notes of ${job.data.user.id} ...`);
 
-		const user = await this.prismaService.client.user.findUnique({ where: { id: job.data.user.id } });
-		if (user == null) {
-			return;
-		}
+		const user = await this.prismaService.client.user.findUnique({
+			where: { id: job.data.user.id },
+			include: { note: { include: { poll: true }, orderBy: { id: 'asc' } } },
+		});
+		if (user === null) return;
+
+		const notes = user.note;
+		const allFileIds = unique(notes.map((note) => note.fileIds).flat());
+		const allFiles = await this.driveFileEntityService.packManyByIdsMap(
+			allFileIds,
+		);
+		const content = notes.map((note) => ({
+			...pick(note, [
+				'id',
+				'text',
+				'createdAt',
+				'fileIds',
+				'replyId',
+				'renoteId',
+				'cw',
+				'visibility',
+				'visibleUserIds',
+				'localOnly',
+				'reactionAcceptance',
+			]),
+			files: note.fileIds.map((fileId) => allFiles.get(fileId)),
+			poll: note.poll,
+		}));
 
 		// Create temp file
 		const [path, cleanup] = await createTemp();
-
 		this.logger.info(`Temp file is ${path}`);
+		fs.writeFileSync(path, JSON.stringify(content));
+		this.logger.succ(`Exported to: ${path}`);
 
-		try {
-			const stream = fs.createWriteStream(path, { flags: 'a' });
+		const fileName =
+			'notes-' + dateFormat(new Date(), 'yyyy-MM-dd-HH-mm-ss') + '.json';
+		const driveFile = await this.driveService.addFile({
+			user,
+			path,
+			name: fileName,
+			force: true,
+			ext: 'json',
+		});
+		this.logger.succ(`Exported to: ${driveFile.id}`);
 
-			const write = (text: string): Promise<void> => {
-				return new Promise<void>((res, rej) => {
-					stream.write(text, err => {
-						if (err) {
-							this.logger.error(err);
-							rej(err);
-						} else {
-							res();
-						}
-					});
-				});
-			};
-
-			await write('[');
-
-			let exportedNotesCount = 0;
-			let cursor: note['id'] | null = null;
-
-			while (true) {
-				const notes: note[] = await this.prismaService.client.note.findMany({
-					where: {
-						userId: user.id,
-						...(cursor ? { id: { gt: cursor } } : {}),
-					},
-					take: 100,
-					orderBy: { id: 'asc' },
-				});
-
-				if (notes.length === 0) {
-					job.updateProgress(100);
-					break;
-				}
-
-				cursor = notes.at(-1)?.id ?? null;
-
-				for (const note of notes) {
-					let poll: poll | undefined;
-					if (note.hasPoll) {
-						poll = await this.prismaService.client.poll.findUniqueOrThrow({ where: { noteId: note.id } });
-					}
-					const files = await this.driveFileEntityService.packManyByIds(note.fileIds);
-					const content = JSON.stringify(serialize(note, poll, files));
-					const isFirst = exportedNotesCount === 0;
-					await write(isFirst ? content : ',\n' + content);
-					exportedNotesCount++;
-				}
-
-				const total = await this.prismaService.client.note.count({
-					where: { userId: user.id },
-				});
-
-				job.updateProgress(exportedNotesCount / total);
-			}
-
-			await write(']');
-
-			stream.end();
-			this.logger.succ(`Exported to: ${path}`);
-
-			const fileName = 'notes-' + dateFormat(new Date(), 'yyyy-MM-dd-HH-mm-ss') + '.json';
-			const driveFile = await this.driveService.addFile({ user, path, name: fileName, force: true, ext: 'json' });
-
-			this.logger.succ(`Exported to: ${driveFile.id}`);
-		} finally {
-			cleanup();
-		}
+		cleanup();
 	}
-}
-
-function serialize(note: note, poll: poll | null = null, files: z.infer<typeof DriveFileSchema>[]): Record<string, unknown> {
-	return {
-		id: note.id,
-		text: note.text,
-		createdAt: note.createdAt,
-		fileIds: note.fileIds,
-		files: files,
-		replyId: note.replyId,
-		renoteId: note.renoteId,
-		poll: poll,
-		cw: note.cw,
-		visibility: note.visibility,
-		visibleUserIds: note.visibleUserIds,
-		localOnly: note.localOnly,
-		reactionAcceptance: note.reactionAcceptance,
-	};
 }
