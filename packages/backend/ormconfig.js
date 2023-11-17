8e3fbfbb8ea1e38d821b8309e7c2da1d45723b7e
okayurisotto <okayurisotto@proton.me>
2023-11-17 12:54:17 +0900

    backend: update `ormconfig.js`
    合わせて`check_connect.js`を削除した。
    `check_connect.js`ではビルドされたbackendの`loadConfig`関数を直接呼び出しており、とても行儀が悪かった。
    もしこういったことが必要なら、backend内にその処理を入れればいいと考える。
    （必要になったときに実装予定。）

--- packages/backend/ormconfig.js
+++ packages/backend/ormconfig.js
@@ -1,17 +1,23 @@
+// @ts-check
+
+import { NestFactory } from '@nestjs/core'
 import { DataSource } from 'typeorm';
-import { loadConfig } from './built/config.js';
-import { entities } from './built/createPostgresDataSource.js';
+import { GlobalModule } from './built/GlobalModule.js';
+import { ConfigLoaderService } from './built/ConfigLoaderService.js';
+import { entities } from './built/core/TypeORMService.js';
 
-const config = loadConfig();
+export default (async () => {
+	const app = await NestFactory.createApplicationContext(GlobalModule);
+	const configLoaderService = await app.resolve(ConfigLoaderService);
 
-export default new DataSource({
-	type: 'postgres',
-	host: config.db.host,
-	port: config.db.port,
-	username: config.db.user,
-	password: config.db.pass,
-	database: config.db.db,
-	extra: config.db.extra,
-	entities: entities,
-	migrations: ['migration/*.js'],
-});
+	return new DataSource({
+		type: 'postgres',
+		host: configLoaderService.data.db.host,
+		port: configLoaderService.data.db.port,
+		username: configLoaderService.data.db.user,
+		password: configLoaderService.data.db.pass,
+		database: configLoaderService.data.db.db,
+		entities: entities,
+		migrations: ['migration/*.js'],
+	});
+})();
