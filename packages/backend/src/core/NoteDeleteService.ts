a4d0d6b19aa10c7fcc1782e73a64374667c620d7
okayurisotto <okayurisotto@proton.me>
2023-11-24 13:19:55 +0900

    backend: refactor `NoteDeleteService`

--- packages/backend/src/core/NoteDeleteService.ts
+++ packages/backend/src/core/NoteDeleteService.ts
@@ -15,7 +15,8 @@ import { PrismaService } from '@/core/PrismaService.js';
 import { ConfigLoaderService } from '@/ConfigLoaderService.js';
 import { UserEntityUtilService } from './entities/UserEntityUtilService.js';
 import { RenoteCountService } from './entities/RenoteCountService.js';
-import type { note, user } from '@prisma/client';
+import type { Prisma, note, user } from '@prisma/client';
+import type { IActivity } from './activitypub/type.js';
 
 @Injectable()
 export class NoteDeleteService {
@@ -37,11 +38,13 @@ export class NoteDeleteService {
 	) {}
 
 	/**
-	 * 投稿を削除します。
-	 * @param user 投稿者
-	 * @param note 投稿
+	 * ノートを削除する。
+	 *
+	 * @param user ノート作成ユーザー
+	 * @param note ノート
+	 * @param quiet `true`だった場合、アクティビティ配送やチャート更新などを行わない。
 	 */
-	async delete(
+	public async delete(
 		user: {
 			id: user['id'];
 			uri: user['uri'];
@@ -52,28 +55,31 @@ export class NoteDeleteService {
 		quiet = false,
 	): Promise<void> {
 		const deletedAt = new Date();
-		const cascadingNotes = await this.findCascadingNotes(note);
+		const cascadingNotes = await this.findCascadingNotes(note.id);
 
-		// この投稿を除く指定したユーザーによる指定したノートのリノートが存在しないとき
-		if (
-			note.renoteId &&
-			(await this.renoteCountService.countSameRenotes(
+		// 当該ノートがリノートで、なおかつ当該ノートを除く同一ユーザーによる同一ノートのリノートが存在しなかった場合、renoteCountとscoreを減少させる。
+		if (note.renoteId) {
+			const sameRenotesCount = await this.renoteCountService.countSameRenotes(
 				user.id,
 				note.renoteId,
 				note.id,
-			)) === 0
-		) {
-			this.prismaService.client.note.update({
-				where: { id: note.renoteId },
-				data: { renoteCount: { decrement: 1 } },
-			});
-			if (!user.isBot)
-				this.prismaService.client.note.update({
-					where: { id: note.renoteId },
-					data: { score: { decrement: 1 } },
-				});
+			);
+			if (sameRenotesCount === 0) {
+				if (user.isBot) {
+					await this.prismaService.client.note.update({
+						where: { id: note.renoteId },
+						data: { renoteCount: { decrement: 1 } },
+					});
+				} else {
+					await this.prismaService.client.note.update({
+						where: { id: note.renoteId },
+						data: { renoteCount: { decrement: 1 }, score: { decrement: 1 } },
+					});
+				}
+			}
 		}
 
+		// 当該ノートがリプライだった場合、repliesCountを減少させる。
 		if (note.replyId) {
 			await this.prismaService.client.note.update({
 				where: { id: note.replyId },
@@ -82,109 +88,120 @@ export class NoteDeleteService {
 		}
 
 		if (!quiet) {
+			const meta = await this.metaService.fetch();
+
+			// 削除イベントをRedisへ送信する。
 			this.globalEventService.publishNoteStream(note.id, 'deleted', {
 				deletedAt: deletedAt,
 			});
 
-			//#region ローカルの投稿なら削除アクティビティを配送
+			// 当該ノートがローカルのものだった場合、削除アクティビティを配送する。
 			if (this.userEntityUtilService.isLocalUser(user) && !note.localOnly) {
-				let renote: note | null = null;
-
-				// if deletd note is renote
 				if (
 					note.renoteId &&
 					note.text == null &&
 					!note.hasPoll &&
-					(note.fileIds == null || note.fileIds.length === 0)
+					note.fileIds.length === 0
 				) {
-					renote = await this.prismaService.client.note.findUnique({
-						where: { id: note.renoteId },
-					});
-				}
+					// 当該ノートがリノートだった場合、undoとして配送する。
+					const renote = await this.prismaService.client.note.findUniqueOrThrow(
+						{ where: { id: note.renoteId } },
+					);
+					const content = this.apRendererService.addContext(
+						this.apRendererService.renderUndo(
+							this.apRendererService.renderAnnounce(
+								renote.uri ??
+									`${this.configLoaderService.data.url}/notes/${renote.id}`,
+								note,
+							),
+							user,
+						),
+					);
 
-				const content = this.apRendererService.addContext(
-					renote
-						? this.apRendererService.renderUndo(
-								this.apRendererService.renderAnnounce(
-									renote.uri ??
-										`${this.configLoaderService.data.url}/notes/${renote.id}`,
-									note,
-								),
-								user,
-						  )
-						: this.apRendererService.renderDelete(
+					await this.deliverToConcerned(user, note, content);
+				} else {
+					// 当該ノートがリノートではなかった場合、deleteとして配送する。
+					const content = this.apRendererService.addContext(
+						this.apRendererService.renderDelete(
+							this.apRendererService.renderTombstone(
+								`${this.configLoaderService.data.url}/notes/${note.id}`,
+							),
+							user,
+						),
+					);
+
+					await this.deliverToConcerned(user, note, content);
+				}
+			}
+
+			// 当該ノートを参照するノートに関しても、削除アクティビティを配送する。
+			await Promise.all(
+				cascadingNotes
+					.filter((note) => !note.localOnly)
+					.filter((note) => note.userHost == null)
+					.map(async (note) => {
+						if (!this.userEntityUtilService.isLocalUser(note.user)) {
+							return;
+						}
+						const content = this.apRendererService.addContext(
+							this.apRendererService.renderDelete(
 								this.apRendererService.renderTombstone(
 									`${this.configLoaderService.data.url}/notes/${note.id}`,
 								),
-								user,
-						  ),
-				);
+								note.user,
+							),
+						);
+						await this.deliverToConcerned(note.user, note, content);
+					}),
+			);
 
-				this.deliverToConcerned(user, note, content);
-			}
-
-			// also deliever delete activity to cascaded notes
-			const federatedLocalCascadingNotes = cascadingNotes.filter(
-				(note) => !note.localOnly && note.userHost == null,
-			); // filter out local-only notes
-			for (const cascadingNote of federatedLocalCascadingNotes) {
-				if (!cascadingNote.user) continue;
-				if (!this.userEntityUtilService.isLocalUser(cascadingNote.user)) {
-					continue;
-				}
-				const content = this.apRendererService.addContext(
-					this.apRendererService.renderDelete(
-						this.apRendererService.renderTombstone(
-							`${this.configLoaderService.data.url}/notes/${cascadingNote.id}`,
-						),
-						cascadingNote.user,
-					),
-				);
-				this.deliverToConcerned(cascadingNote.user, cascadingNote, content);
-			}
-			//#endregion
-
-			const meta = await this.metaService.fetch();
-
-			this.notesChart.update(note, false);
+			// ユーザーごとのノートのチャートを更新する。
+			await this.notesChart.update(note, false);
 			if (meta.enableChartsForRemoteUser || user.host == null) {
 				this.perUserNotesChart.update(user, note, false);
 			}
 
+			// 当該ノートの作成者がリモートユーザーだった場合、インスタンスのチャートを更新する。
 			if (this.userEntityUtilService.isRemoteUser(user)) {
-				this.federatedInstanceService.fetch(user.host).then(async (i) => {
-					this.prismaService.client.instance.update({
-						where: { id: i.id },
-						data: { notesCount: { decrement: 1 } },
-					});
-					if (
-						(await this.metaService.fetch()).enableChartsForFederatedInstances
-					) {
-						this.instanceChart.updateNote(i.host, note, false);
-					}
+				const instance = await this.federatedInstanceService.fetch(user.host);
+				await this.prismaService.client.instance.update({
+					where: { id: instance.id },
+					data: { notesCount: { decrement: 1 } },
 				});
+				if (meta.enableChartsForFederatedInstances) {
+					await this.instanceChart.updateNote(instance.host, note, false);
+				}
 			}
 		}
 
-		for (const cascadingNote of cascadingNotes) {
-			this.searchService.unindexNote(cascadingNote);
-		}
-		this.searchService.unindexNote(note);
+		// 検索用のインデックスから当該ノート及び関連するすべてのノートを削除する。
+		await Promise.all(
+			[note, ...cascadingNotes].map(async (note) => {
+				await this.searchService.unindexNote(note);
+			}),
+		);
 
+		// データベースから当該ノートを削除する。
+		// 関連するノートの削除はデータベース側がCASCADE削除するため明示的な指定は必要ない。
 		await this.prismaService.client.note.delete({
-			where: {
-				id: note.id,
-				userId: user.id,
-			},
+			where: { id: note.id, userId: user.id },
 		});
 	}
 
+	/**
+	 * あるノートと関連したすべてのノートを再帰的に取得する。
+	 * ここでいう「関連したノート」とは、「当該ノートへのリプライであるノート」と「当該ノートへの引用リノートであるノート」のこと。
+	 *
+	 * ノートの削除において、当該ノートと関連したノートの削除はデータベース側で（CASCADE設定によって）自動的に行われます。
+	 * しかし実際の削除では、事前に削除アクティビティを配送するなどの処理が必要です。
+	 * よって、データベースがCASCADE削除するノートをアプリケーション側でもこのように取得できるようにする必要があります。
+	 */
 	private async findCascadingNotes(
-		note: note,
-	): Promise<(note & { user: user | null })[]> {
+		noteId: string,
+	): Promise<(note & { user: user })[]> {
 		const recursive = async (
 			noteId: string,
-		): Promise<(note & { user: user | null })[]> => {
+		): Promise<(note & { user: user })[]> => {
 			const replies = await this.prismaService.client.note.findMany({
 				where: {
 					OR: [{ replyId: noteId }, { renoteId: noteId, text: { not: null } }],
@@ -198,13 +215,11 @@ export class NoteDeleteService {
 			].flat();
 		};
 
-		const cascadingNotes = await recursive(note.id);
-
-		return cascadingNotes;
+		return await recursive(noteId);
 	}
 
 	private async getMentionedRemoteUsers(note: note): Promise<RemoteUser[]> {
-		const where = [];
+		const where: Prisma.userWhereInput[] = [];
 
 		// mention / reply / dm
 		const uris = (
@@ -229,13 +244,19 @@ export class NoteDeleteService {
 	private async deliverToConcerned(
 		user: { id: LocalUser['id']; host: null },
 		note: note,
-		content: any,
+		content: IActivity,
 	): Promise<void> {
-		this.apDeliverManagerService.deliverToFollowers(user, content);
-		this.relayService.deliverToRelays(user, content);
+		await this.apDeliverManagerService.deliverToFollowers(user, content);
+		await this.relayService.deliverToRelays(user, content);
 		const remoteUsers = await this.getMentionedRemoteUsers(note);
-		for (const remoteUser of remoteUsers) {
-			this.apDeliverManagerService.deliverToUser(user, content, remoteUser);
-		}
+		await Promise.all(
+			remoteUsers.map(async (remoteUser) => {
+				await this.apDeliverManagerService.deliverToUser(
+					user,
+					content,
+					remoteUser,
+				);
+			}),
+		);
 	}
 }
