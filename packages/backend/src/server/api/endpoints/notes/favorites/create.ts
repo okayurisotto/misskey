30e7e158f81efcd1b7fd74c155f69db8219c25b8
okayurisotto <okayurisotto@proton.me>
2023-11-24 13:42:52 +0900

    backend: refactor `/api/notes/favorites/create`

--- packages/backend/src/server/api/endpoints/notes/favorites/create.ts
+++ packages/backend/src/server/api/endpoints/notes/favorites/create.ts
@@ -4,11 +4,9 @@ import ms from 'ms';
 import { noSuchNote________, alreadyFavorited_ } from '@/server/api/errors.js';
 import { IdService } from '@/core/IdService.js';
 import { Endpoint } from '@/server/api/abstract-endpoint.js';
-import { GetterService } from '@/server/api/GetterService.js';
 import { AchievementService } from '@/core/AchievementService.js';
 import { MisskeyIdSchema } from '@/models/zod/misc.js';
 import { PrismaService } from '@/core/PrismaService.js';
-import { ApiError } from '../../../error.js';
 
 export const meta = {
 	tags: ['notes', 'favorites'],
@@ -37,46 +35,26 @@ export default class extends Endpoint<
 	z.ZodType<void>
 > {
 	constructor(
-		private readonly idService: IdService,
-		private readonly getterService: GetterService,
 		private readonly achievementService: AchievementService,
+		private readonly idService: IdService,
 		private readonly prismaService: PrismaService,
 	) {
 		super(meta, paramDef, async (ps, me) => {
-			// Get favoritee
-			const note = await this.getterService.getNote(ps.noteId).catch((err) => {
-				if (err.id === '9725d0ce-ba28-4dde-95a7-2cbb2c15de24') {
-					throw new ApiError(meta.errors.noSuchNote);
-				}
-				throw err;
-			});
-
-			// if already favorited
-			const exist =
-				(await this.prismaService.client.note_favorite.count({
-					where: {
-						noteId: note.id,
-						userId: me.id,
-					},
-					take: 1,
-				})) > 0;
-
-			if (exist) {
-				throw new ApiError(meta.errors.alreadyFavorited);
-			}
-
-			// Create favorite
-			await this.prismaService.client.note_favorite.create({
+			const favorite = await this.prismaService.client.note_favorite.create({
 				data: {
 					id: this.idService.genId(),
 					createdAt: new Date(),
-					noteId: note.id,
+					noteId: ps.noteId,
 					userId: me.id,
 				},
+				include: { note: true },
 			});
 
-			if (note.userHost == null && note.userId !== me.id) {
-				this.achievementService.create(note.userId, 'myNoteFavorited1');
+			if (favorite.note.userHost === null && favorite.note.userId !== me.id) {
+				await this.achievementService.create(
+					favorite.note.userId,
+					'myNoteFavorited1',
+				);
 			}
 		});
 	}
