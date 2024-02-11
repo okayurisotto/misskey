1a9eb22f601603e1960c54726dfce02444b019d5
okayurisotto <okayurisotto@proton.me>
2024-02-11 23:38:20 +0900

    backend: refactor `/api/notes/clips`

--- packages/backend/src/server/api/endpoints/notes/clips.ts
+++ packages/backend/src/server/api/endpoints/notes/clips.ts
@@ -1,20 +1,16 @@
 import { z } from 'zod';
 import { Injectable } from '@nestjs/common';
-import { noSuchNote_____ } from '@/server/api/errors.js';
 import { Endpoint } from '@/server/api/abstract-endpoint.js';
 import { ClipEntityService } from '@/core/entities/ClipEntityService.js';
-import { GetterService } from '@/server/api/GetterService.js';
 import { ClipSchema } from '@/models/zod/ClipSchema.js';
 import { MisskeyIdSchema } from '@/models/zod/misc.js';
 import { PrismaService } from '@/core/PrismaService.js';
-import { ApiError } from '../../error.js';
 
 const res = z.array(ClipSchema);
 export const meta = {
 	tags: ['clips', 'notes'],
 	requireCredential: false,
 	res,
-	errors: { noSuchNote: noSuchNote_____ },
 } as const;
 
 export const paramDef = z.object({
@@ -30,26 +26,17 @@ export default class extends Endpoint<
 > {
 	constructor(
 		private readonly clipEntityService: ClipEntityService,
-		private readonly getterService: GetterService,
 		private readonly prismaService: PrismaService,
 	) {
 		super(meta, paramDef, async (ps, me) => {
-			const note = await this.getterService.getNote(ps.noteId).catch((err) => {
-				if (err.id === '9725d0ce-ba28-4dde-95a7-2cbb2c15de24')
-					throw new ApiError(meta.errors.noSuchNote);
-				throw err;
+			const note = await this.prismaService.client.note.findUniqueOrThrow({
+				where: { id: ps.noteId },
+				include: { clipNotes: { include: { clip: true } } },
 			});
 
-			const clipNotes = await this.prismaService.client.clipNote.findMany({
-				where: { noteId: note.id },
-			});
-
-			const clips = await this.prismaService.client.clip.findMany({
-				where: {
-					id: { in: clipNotes.map((x) => x.clipId) },
-					isPublic: true,
-				},
-			});
+			const clips = note.clipNotes
+				.flatMap((clipNote) => clipNote.clip)
+				.filter((clip) => clip.isPublic);
 
 			return await Promise.all(
 				clips.map((clip) => this.clipEntityService.pack(clip, me)),
