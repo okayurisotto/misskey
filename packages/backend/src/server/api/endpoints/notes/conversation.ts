84256f4077f6118be0b8766f466eff860137b3a6
okayurisotto <okayurisotto@proton.me>
2024-02-11 23:38:35 +0900

    backend: refactor `/api/notes/conversation`

--- packages/backend/src/server/api/endpoints/notes/conversation.ts
+++ packages/backend/src/server/api/endpoints/notes/conversation.ts
@@ -1,21 +1,17 @@
 import { z } from 'zod';
 import { Injectable } from '@nestjs/common';
-import { noSuchNote______ } from '@/server/api/errors.js';
 import { Endpoint } from '@/server/api/abstract-endpoint.js';
 import { NoteEntityPackService } from '@/core/entities/NoteEntityPackService.js';
-import { GetterService } from '@/server/api/GetterService.js';
 import { NoteSchema } from '@/models/zod/NoteSchema.js';
 import { MisskeyIdSchema, limit } from '@/models/zod/misc.js';
 import { PrismaService } from '@/core/PrismaService.js';
-import { ApiError } from '../../error.js';
 import type { Note } from '@prisma/client';
 
-const res = z.array(NoteSchema);
+const res = NoteSchema.array();
 export const meta = {
 	tags: ['notes'],
 	requireCredential: false,
 	res,
-	errors: { noSuchNote: noSuchNote______ },
 } as const;
 
 export const paramDef = z.object({
@@ -33,44 +29,43 @@ export default class extends Endpoint<
 > {
 	constructor(
 		private readonly noteEntityService: NoteEntityPackService,
-		private readonly getterService: GetterService,
 		private readonly prismaService: PrismaService,
 	) {
 		super(meta, paramDef, async (ps, me) => {
-			const note = await this.getterService.getNote(ps.noteId).catch((err) => {
-				if (err.id === '9725d0ce-ba28-4dde-95a7-2cbb2c15de24') {
-					throw new ApiError(meta.errors.noSuchNote);
-				}
-				throw err;
+			const note = await this.prismaService.client.note.findUniqueOrThrow({
+				where: { id: ps.noteId },
 			});
 
-			const conversation: Note[] = [];
-			let i = 0;
+			if (note.replyId === null) return [];
 
-			const get = async (id: any): Promise<void> => {
-				i++;
-				const p = await this.prismaService.client.note.findUnique({
+			const get = async (
+				id: string,
+				conversation: Note[] = [],
+				depth = 0,
+			): Promise<Note[]> => {
+				const note = await this.prismaService.client.note.findUnique({
 					where: { id },
 				});
-				if (p == null) return;
+				if (note === null) {
+					return conversation;
+				}
 
-				if (i > ps.offset) {
-					conversation.push(p);
+				if (depth >= ps.offset) {
+					conversation.push(note);
 				}
 
 				if (conversation.length === ps.limit) {
-					return;
+					return conversation;
 				}
 
-				if (p.replyId) {
-					await get(p.replyId);
+				if (note.replyId) {
+					return await get(note.replyId, conversation, depth + 1);
 				}
+
+				return conversation;
 			};
 
-			if (note.replyId) {
-				await get(note.replyId);
-			}
-
+			const conversation = await get(note.replyId);
 			return await this.noteEntityService.packMany(conversation, me);
 		});
 	}
