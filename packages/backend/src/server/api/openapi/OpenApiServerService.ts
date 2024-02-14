84d4f4499fec56e392ba40ad1e5c164dcb036aaa
okayurisotto <okayurisotto@proton.me>
2024-02-14 14:14:25 +0900

    backend: `BACKEND_STATIC_ASSETS_DIR`を使うように

--- packages/backend/src/server/api/openapi/OpenApiServerService.ts
+++ packages/backend/src/server/api/openapi/OpenApiServerService.ts
@@ -1,22 +1,21 @@
-import { fileURLToPath } from 'node:url';
 import { Injectable } from '@nestjs/common';
 import { ConfigLoaderService } from '@/ConfigLoaderService.js';
+import { BACKEND_STATIC_ASSETS_DIR } from '@/paths.js';
 import { generateFullOpenApiSpec } from './gen-spec.js';
 import type { FastifyInstance, FastifyPluginOptions } from 'fastify';
 
-const staticAssets = fileURLToPath(new URL('../../../../assets/', import.meta.url));
-
 @Injectable()
 export class OpenApiServerService {
-	constructor(
-		private readonly configLoaderService: ConfigLoaderService,
-	) {
-	}
+	constructor(private readonly configLoaderService: ConfigLoaderService) {}
 
-	public createServer(fastify: FastifyInstance, _options: FastifyPluginOptions, done: (err?: Error) => void): void {
+	public createServer(
+		fastify: FastifyInstance,
+		_options: FastifyPluginOptions,
+		done: (err?: Error) => void,
+	): void {
 		fastify.get('/api-doc', async (_request, reply) => {
 			reply.header('Cache-Control', 'public, max-age=86400');
-			return await reply.sendFile('/redoc.html', staticAssets);
+			return await reply.sendFile('/redoc.html', BACKEND_STATIC_ASSETS_DIR);
 		});
 		fastify.get('/api.json', (_request, reply) => {
 			reply.header('Cache-Control', 'public, max-age=600');
