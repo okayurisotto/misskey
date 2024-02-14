74154bdf2df35ad388627c6477e94e6fafd07dbb
okayurisotto <okayurisotto@proton.me>
2024-02-14 10:15:32 +0900

    backend: refactor user list import

--- packages/backend/src/queue/processors/ImportUserListsProcessorService.ts
+++ packages/backend/src/queue/processors/ImportUserListsProcessorService.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { z } from 'zod';
-import { User, user_list } from '@prisma/client';
+import { Prisma, User, user_list } from '@prisma/client';
 import * as Acct from '@/misc/acct.js';
 import { RemoteUserResolveService } from '@/core/RemoteUserResolveService.js';
 import { DownloadService } from '@/core/DownloadService.js';
@@ -9,6 +9,7 @@ import { IdService } from '@/core/IdService.js';
 import { UtilityService } from '@/core/UtilityService.js';
 import { PrismaService } from '@/core/PrismaService.js';
 import { unique } from '@/misc/prelude/array.js';
+import { isNotNull } from '@/misc/is-not-null.js';
 import { QueueLoggerService } from '../QueueLoggerService.js';
 import type * as Bull from 'bullmq';
 import type { DbUserImportJobData } from '../types.js';
@@ -16,22 +17,6 @@ import type { DbUserImportJobData } from '../types.js';
 export const ExportedUserListsSchema = z.array(
 	z.tuple([z.string(), z.string()]),
 );
-
-const getUserFromAcct = (users: User[]) => {
-	return (acct: Acct.Acct): User | undefined => {
-		return users.find((user) => {
-			if (user.usernameLower !== acct.username.toLowerCase()) {
-				return false;
-			}
-
-			if (user.host !== acct.host) {
-				return false;
-			}
-
-			return true;
-		});
-	};
-};
 
 @Injectable()
 export class ImportUserListsProcessorService {
@@ -50,10 +35,32 @@ export class ImportUserListsProcessorService {
 			this.queueLoggerService.logger.createSubLogger('import-user-lists');
 	}
 
-	public async process(job: Bull.Job<DbUserImportJobData>): Promise<void> {
-		const now = new Date();
+	private isLocalUserAcct(acct: Acct.Acct): boolean {
+		return acct.host === null || this.utilityService.isSelfHost(acct.host);
+	}
 
+	private getUserFromAcct(users: User[], acct: Acct.Acct): User | undefined {
+		return users.find((user) => {
+			if (user.usernameLower !== acct.username.toLowerCase()) {
+				return false;
+			}
+
+			if (this.isLocalUserAcct(acct)) {
+				if (user.host !== null) return false;
+			} else {
+				if (user.host !== acct.host) return false;
+			}
+
+			return true;
+		});
+	}
+
+	public async process(job: Bull.Job<DbUserImportJobData>): Promise<void> {
 		this.logger.info(`Importing user lists of ${job.data.user.id} ...`);
+
+		// 準備 //
+
+		const now = new Date();
 
 		const me = await this.prismaService.client.user.findUnique({
 			where: { id: job.data.user.id },
@@ -68,107 +75,154 @@ export class ImportUserListsProcessorService {
 
 		const csv = await this.downloadService.downloadTextFile(file.url);
 
-		// CSVをパースし、`Map<user_list["name"], Acct.Acct[]>`にする
+		// CSVをパースする //
 
-		const data = new Map(
-			[
-				...ExportedUserListsSchema.parse(
-					csv
-						.trim()
-						.split('\n')
-						.map((line) => line.split(',')),
-				).reduce<Map<string, Set<string>>>((acc, [list, acct]) => {
-					return acc.set(list, (acc.get(list) ?? new Set()).add(acct));
-				}, new Map()),
-			].map(([listName, users]) => {
-				return [listName, [...users].map((acct) => Acct.parse(acct))];
-			}),
-		);
+		const data = ((): Map<string, Set<Acct.Acct>> => {
+			const lines = csv
+				.trim()
+				.split('\n')
+				.map((line) => line.split(','));
 
-		// 足りない`user_list`を作る
-		// 同じ名前の`user_list`がすでにあればそれを使うため新規作成はしない（この挙動どうなの？）
+			const entries = ExportedUserListsSchema.parse(lines);
 
-		const userListCreateManyData = [...data.keys()]
-			.filter((listName) => {
-				return me.userLists.every((list) => list.name !== listName);
-			})
-			.map((listName) => ({
-				id: this.idService.genId(),
-				createdAt: now,
-				name: listName,
-				userId: me.id,
-			}));
-		await this.prismaService.client.user_list.createMany({
-			data: userListCreateManyData,
-		});
+			return entries.reduce((acc, [listName, acct]) => {
+				const prevItems = acc.get(listName);
+				const defaultItems = new Set<Acct.Acct>();
+				const item = Acct.parse(acct);
 
-		// 足りない`user`を解決しつつ全`user`を取得
+				return acc.set(listName, (prevItems ?? defaultItems).add(item));
+			}, new Map<string, Set<Acct.Acct>>());
+		})();
 
-		const allUsers = await Promise.all(
-			unique([...data.values()].map((set) => [...set]).flat()).map(
-				async ({ username, host }) => {
-					const user = await this.prismaService.client.user.findFirst({
-						where: {
-							host:
-								host === null || this.utilityService.isSelfHost(host)
-									? null
-									: this.utilityService.toPuny(host),
-							usernameLower: username.toLowerCase(),
-						},
-					});
-					if (user !== null) return user;
+		// 足りないユーザーリストを新規に作成する //
 
-					return await this.remoteUserResolveService.resolveUser(
-						username,
-						host,
-					);
-				},
-			),
-		);
+		{
+			const allListNames = new Set(me.userLists.map((list) => list.name));
 
-		// 既存の`user_list`をすべて取得
+			const userListCreateManyData = [...data.keys()]
+				.filter((listName) => {
+					// 同じ名前の`user_list`がすでにあればそれを使うため新規作成はしない
+					return !allListNames.has(listName);
+				})
+				.map((listName) => ({
+					id: this.idService.genId(),
+					createdAt: now,
+					name: listName,
+					userId: me.id,
+				}));
+
+			await this.prismaService.client.user_list.createMany({
+				data: userListCreateManyData,
+			});
+		}
+
+		// 一旦すべてのユーザーを取得・解決する //
+
+		const allUsers = await (async (): Promise<User[]> => {
+			// 準備 //
+
+			const allAccts = unique([...data.values()].flatMap((set) => [...set]));
+
+			// すべてのユーザーについてダメ元でデータベースに一括で問い合わせる //
+
+			const userWhereInputs = allAccts.map((acct): Prisma.UserWhereInput => {
+				if (this.isLocalUserAcct(acct)) {
+					return {
+						host: null,
+						usernameLower: acct.username.toLowerCase(),
+					};
+				} else {
+					if (acct.host === null) throw new Error();
+
+					return {
+						host: this.utilityService.toPuny(acct.host),
+						usernameLower: acct.username.toLowerCase(),
+					};
+				}
+			});
+
+			/** データベースから一括取得したユーザー情報 */
+			const dbUsers = await this.prismaService.client.user.findMany({
+				where: { OR: userWhereInputs },
+			});
+
+			// すべてのユーザーを取得・解決する //
+
+			const users = await Promise.all(
+				allAccts.map(async (acct) => {
+					const user = this.getUserFromAcct(dbUsers, acct);
+					if (user !== undefined) {
+						// すでにデータベースにユーザーが存在するなら御の字
+						return user;
+					} else {
+						// データベースにないということは未解決のリモートユーザーということなので解決する
+						return await this.remoteUserResolveService.resolveUser(
+							acct.username,
+							acct.host,
+						);
+					}
+				}),
+			);
+
+			// 完了 //
+
+			return users;
+		})();
+
+		// 既存のユーザーリストを（先程新規に作成した分も含めて）すべて取得する //
 
 		const allLists = await this.prismaService.client.user_list.findMany({
 			where: { userId: me.id },
 			include: { user_list_joining: true },
 		});
-		const allListsByName = new Map(allLists.map((list) => [list.name, list]));
 
-		// 足りない`user_list_joinging`を作る
+		const allListsByName = new Map(
+			allLists.map((list) => {
+				// 同じ名前のユーザーリストが複数あった場合、いずれかが選ばれる
+				// どのユーザーリストが選ばれるかはデータベースからの取得順による
+				// （データベースからの取得順（`orderBy`）は未定義）
+				return [list.name, list];
+			}),
+		);
+
+		// 足りない`user_list_joinging`を作る //
 
 		const joiningCreateManyData = [...data]
-			.map<{ user: User; user_list: user_list }[]>(([listName, accts]) => {
+			.flatMap(([listName, accts]) => {
+				return [...accts].map((acct) => [listName, acct] as const);
+			})
+			.map<{ user: User; user_list: user_list } | null>(([listName, acct]) => {
 				const user_list = allListsByName.get(listName);
 				if (user_list === undefined) {
-					// 取得もしくは作成していたはずの`user_list`がない
+					// 取得もしくは作成していたはずのユーザーリストがない
 					throw new Error('Cannot find user list.');
 				}
 
-				const users = accts.map((acct) => {
-					const user = getUserFromAcct(allUsers)(acct);
-					if (user === undefined) {
-						// 取得もしくは解決していたはずの`user`がない
-						throw new Error('Cannot find user.');
-					}
-					return user;
-				});
+				const user = this.getUserFromAcct(allUsers, acct);
+				if (user === undefined) {
+					// 取得もしくは解決していたはずのユーザーがない
+					throw new Error('Cannot find user.');
+				}
 
-				return users
-					.filter((user) => {
-						// すでにある`user_list_joining`のすべてが当該`user`のものでない場合：足りていない
-						return user_list.user_list_joining.every((joining) => {
-							return joining.userId !== user.id;
-						});
-					})
-					.map((user) => ({ user, user_list }));
+				const alreadyExist = user_list.user_list_joining.some((joining) => {
+					return joining.userId === user.id;
+				});
+				if (alreadyExist) {
+					// nothing to do
+					return null;
+				}
+
+				return { user, user_list };
 			})
-			.flat();
+			.filter(isNotNull);
 
 		await Promise.all(
 			joiningCreateManyData.map(async ({ user: target, user_list }) => {
 				await this.userListService.push(target, user_list, me);
 			}),
 		);
+
+		// 完了 //
 
 		this.logger.succ('Imported');
 	}
